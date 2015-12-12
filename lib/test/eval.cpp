a4d74766369a243e6f83ffc4216eee20d2e35f75
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-12 11:55:24 -0500

    Add test cases for evaluation

---
+++ lib/test/eval.cpp
@@ -0,0 +1,40 @@
+#include <catch/catch.hpp>
+
+#include "store.hpp"
+#include "tree.hpp"
+
+TEST_CASE("Double evaluation")
+{
+    Store s;
+    Tree t(&s, s.operation(OP_ADD, s.X(), s.constant(1)));
+
+    t.mode<double>();
+    REQUIRE(t.eval(1.0, 2.0, 3.0) == 2.0);
+}
+
+TEST_CASE("Interval evaluation")
+{
+    Store s;
+    Tree t(&s, s.operation(OP_ADD, s.X(), s.constant(1)));
+
+    t.mode<Interval>();
+
+    Interval arg(1, 2);
+    auto out = t.eval(arg, arg, arg);
+
+    REQUIRE(out.lower() == 2.0);
+    REQUIRE(out.upper() == 3.0);
+}
+
+TEST_CASE("List of doubles evaluation")
+{
+    Store s;
+    Tree t(&s, s.operation(OP_ADD, s.X(), s.constant(1)));
+
+    t.mode<double>();
+
+    std::vector<double> args({1, 2, 3, 4, 5});
+    std::vector<double>  out({2, 3, 4, 5, 6});
+
+    REQUIRE(t.eval(args, args, args) == out);
+}
