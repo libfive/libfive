6dc008c6a9f97c764527edabd24bb86df071628e
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-15 18:15:41 -0500

    More unit tests for rendering

--- lib/test/heightmap.cpp
+++ lib/test/heightmap.cpp
@@ -32,7 +32,94 @@ TEST_CASE("2D rendering of a circle")
         -inf,   0,   0,   0,   0,   0,   0,   0,   0,-inf,
         -inf,-inf,-inf,   0,   0,   0,   0,-inf,-inf,-inf;
 
+    CAPTURE(out);
     REQUIRE((comp == out).all());
+}
+
+TEST_CASE("Render orientation")
+{
+    Store s;
+    Region r({-1, 1}, {-1, 1}, {0, 0}, 5);
+
+    SECTION("Y")
+    {
+        Tree t(&s, s.operation(OP_MAX,
+                   s.operation(OP_SUB,
+                   s.operation(OP_ADD, s.operation(OP_MUL, s.X(), s.X()),
+                                       s.operation(OP_MUL, s.Y(), s.Y())),
+                   s.constant(1)), s.Y()));
+
+        auto out = Heightmap::Render(&t, r);
+
+        Eigen::ArrayXXd comp(10, 10);
+        double inf = std::numeric_limits<double>::infinity();
+        comp <<
+            -inf,-inf,-inf,   0,   0,   0,   0,-inf,-inf,-inf,
+            -inf,   0,   0,   0,   0,   0,   0,   0,   0,-inf,
+            -inf,   0,   0,   0,   0,   0,   0,   0,   0,-inf,
+               0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+               0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+            -inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,
+            -inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,
+            -inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,
+            -inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,
+            -inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf;
+
+        CAPTURE(out);
+        REQUIRE((comp == out).all());
+    }
+
+    SECTION("X")
+    {
+        Tree t(&s, s.operation(OP_MAX,
+                   s.operation(OP_SUB,
+                   s.operation(OP_ADD, s.operation(OP_MUL, s.X(), s.X()),
+                                       s.operation(OP_MUL, s.Y(), s.Y())),
+                   s.constant(1)), s.X()));
+
+        auto out = Heightmap::Render(&t, r);
+
+        Eigen::ArrayXXd comp(10, 10);
+        double inf = std::numeric_limits<double>::infinity();
+        comp <<
+            -inf,-inf,-inf,   0,   0,-inf,-inf,-inf,-inf,-inf,
+            -inf,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+            -inf,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+               0,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+               0,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+               0,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+               0,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+            -inf,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+            -inf,   0,   0,   0,   0,-inf,-inf,-inf,-inf,-inf,
+            -inf,-inf,-inf,   0,   0,-inf,-inf,-inf,-inf,-inf;
+
+        CAPTURE(out);
+        REQUIRE((comp == out).all());
+    }
+}
+
+TEST_CASE("Render shape")
+{
+    Store s;
+    Tree t(&s, s.operation(OP_SUB,
+               s.operation(OP_ADD, s.operation(OP_MUL, s.X(), s.X()),
+                                   s.operation(OP_MUL, s.Y(), s.Y())),
+               s.constant(1)));
+
+    SECTION("X")
+    {
+        Region r({0, 1}, {-1, 1}, {0, 0}, 5);
+        auto out = Heightmap::Render(&t, r);
+        REQUIRE(out.rows() == 10);
+        REQUIRE(out.cols() == 5);
+    }
+    SECTION("Y")
+    {
+        Region r({-1, 1}, {0, 1}, {0, 0}, 5);
+        auto out = Heightmap::Render(&t, r);
+        REQUIRE(out.rows() == 5);
+        REQUIRE(out.cols() == 10);
+    }
 }
 
 TEST_CASE("3D rendering of a sphere")
