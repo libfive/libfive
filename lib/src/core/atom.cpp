9e00081e8533d88642ab914c587f18741c4cd662
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-23 18:26:33 -0500

    Move compile-time array checking to separate function

--- lib/src/core/atom.cpp
+++ lib/src/core/atom.cpp
@@ -1,20 +1,25 @@
 #include "ao/core/atom.hpp"
 #include "ao/core/token.hpp"
 
-/*  Compile-time macro to check array size  */
+////////////////////////////////////////////////////////////////////////////////
+/*  Compile-time checking of object sizes  */
 #define CHECK(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
+void CHECK_SIZES()
+{
+    CHECK(ATOM_ARRAY_BYTES % sizeof(double));
+    CHECK(ATOM_ARRAY_BYTES % sizeof(Interval));
+    CHECK(ATOM_ARRAY_BYTES % sizeof(Gradient));
+    CHECK(sizeof(Interval) == sizeof(double));
+    CHECK(sizeof(Interval) == sizeof(Gradient));
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 Atom::Atom(Token* t)
     : op(t->op), value(t->value), mutable_value(nan("")), flags(0),
       a(t->a ? t->a->atom : nullptr),
       b(t->b ? t->b->atom : nullptr)
 {
-    // Compile-time detection of error conditions
-    CHECK(ATOM_ARRAY_BYTES % sizeof(double));
-    CHECK(ATOM_ARRAY_BYTES % sizeof(Interval));
-    CHECK(ATOM_ARRAY_BYTES % sizeof(Gradient));
-    CHECK(sizeof(Interval) == sizeof(double));
-    CHECK(sizeof(Interval) == sizeof(Gradient));
 
     // Assert that children have atom pointers populated
     assert(t->a ? t->a->atom != nullptr : true);
