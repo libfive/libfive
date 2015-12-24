9d6481d12ccb76ee5ef71ce48bc41df4e67cadcc
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-23 20:03:16 -0500

    Add failing unit test for matrix evaluation

---
+++ lib/test/matrix.cpp
@@ -0,0 +1,37 @@
+#include <catch/catch.hpp>
+
+#include "ao/core/store.hpp"
+#include "ao/core/tree.hpp"
+
+TEST_CASE("Matrix evaluation")
+{
+    Store s;
+    Tree t(&s, s.X());
+
+    SECTION("Identity")
+    {
+        t.setMatrix(glm::mat4(1.0, 0.0, 0.0, 0.0,
+                              0.0, 1.0, 0.0, 0.0,
+                              0.0, 0.0, 1.0, 0.0,
+                              0.0, 0.0, 0.0, 1.0));
+        REQUIRE(t.eval(1.0, 2.0, 3.0) == 1.0);
+    }
+
+    SECTION("Scaling")
+    {
+        t.setMatrix(glm::mat4(0.5, 0.0, 0.0, 0.0,
+                              0.0, 1.0, 0.0, 0.0,
+                              0.0, 0.0, 1.0, 0.0,
+                              0.0, 0.0, 0.0, 1.0));
+        REQUIRE(t.eval(1.0, 2.0, 3.0) == 0.5);
+    }
+
+    SECTION("Offset")
+    {
+        t.setMatrix(glm::mat4(1.0, 0.0, 0.0, 1.5,
+                              0.0, 1.0, 0.0, 0.0,
+                              0.0, 0.0, 1.0, 0.0,
+                              0.0, 0.0, 0.0, 1.0));
+        REQUIRE(t.eval(1.0, 2.0, 3.0) == 2.5);
+    }
+}
