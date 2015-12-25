a4cca44eed39c1d6e01fed7277f7ddebfba3d581
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-25 10:51:05 -0500

    Apply projection matrix to axes

--- lib/inc/ao/ui/window.hpp
+++ lib/inc/ao/ui/window.hpp
@@ -4,6 +4,7 @@
 
 #include "ao/gl/core.hpp"
 #include "ao/gl/frame.hpp"
+#include "ao/gl/axes.hpp"
 
 class Tree;
 
@@ -12,10 +13,37 @@ class Window
 public:
     static bool Show(Tree* tree);
 
+    /*
+     *  Resized window callback
+     */
+    void resized(int w, int h);
+
 protected:
     explicit Window(GLFWwindow* window);
 
+    /*
+     *  Blocking loop updating the window until it is closed
+     */
     void run();
 
+    /*
+     *  Redraw the window with the current state
+     */
+    void draw() const;
+
+    /*
+     *  Projection matrix
+     *  (compensates for window size)
+     */
+    glm::mat4 proj() const;
+
+    /*  Pointer to raw window  */
     GLFWwindow* const window;
+
+    /*  Width and height are of the framebuffer, rather than the window  *
+     *  (to properly cope with high DPI monitors)                        */
+    int width;
+    int height;
+
+    Axes axes;
 };
--- lib/src/ui/window.cpp
+++ lib/src/ui/window.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "ao/ui/window.hpp"
 #include "ao/core/tree.hpp"
 
 #include "ao/gl/core.hpp"
 #include "ao/gl/frame.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
 
 bool Window::Show(Tree* tree)
 {
@@ -23,28 +27,60 @@ bool Window::Show(Tree* tree)
 
 ////////////////////////////////////////////////////////////////////////////////
 
+// Callback wrappers
+static void _resized(GLFWwindow* window, int w, int h)
+{
+    static_cast<Window*>(glfwGetWindowUserPointer(window))->resized(w, h);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 Window::Window(GLFWwindow* window)
     : window(window)
 {
-    // Nothing to do here
+    glfwGetFramebufferSize(window, &width, &height);
+    glfwSetWindowUserPointer(window, this);
+    glfwSetFramebufferSizeCallback(window, _resized);
+}
+
+void Window::resized(int w, int h)
+{
+    width = w;
+    height = h;
+    draw();
+}
+
+glm::mat4 Window::proj() const
+{
+    glm::mat4 m;
+    if (width > height)
+    {
+        m = glm::scale(m, {height/float(width), 1.0, 1.0});
+    }
+    else
+    {
+        m = glm::scale(m, {1.0, width/float(height), 1.0});
+    }
+    return m;
+}
+
+void Window::draw() const
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    axes.draw(proj());
+
+    glfwSwapBuffers(window);
 }
 
 void Window::run()
 {
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        //shaders.Use();
-        //frame.Draw();
-
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
-
         // Poll for and process events
         glfwPollEvents();
-    }
 
-    glfwTerminate();
+        // Redraw the window
+        draw();
+    }
 }
