1dba184e7b50e9cfb3d3687c930ecbf55122b968
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-24 21:00:59 -0500

    Remove oglplus library

--- lib/inc/ao/gl/core.hpp
+++ lib/inc/ao/gl/core.hpp
@@ -3,9 +3,10 @@
 #define GL_VERSION_MAJOR 3
 #define GL_VERSION_MINOR 3
 
+#include <string>
+
 #include <epoxy/gl.h>
 #include <GLFW/glfw3.h>
-#include <oglplus/all.hpp>
 
 /*
  *  Creates a visible window and returns a pointer
