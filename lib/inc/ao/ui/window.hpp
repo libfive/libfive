504fcb211809d6d4c79b471773834c5875514a85
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-25 21:44:44 -0500

    Infrastructure for 3D view (translation implemented)

--- lib/inc/ao/ui/window.hpp
+++ lib/inc/ao/ui/window.hpp
@@ -2,6 +2,10 @@
 
 #include <memory>
 
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
+
 #include "ao/gl/core.hpp"
 #include "ao/gl/frame.hpp"
 #include "ao/gl/axes.hpp"
@@ -17,6 +21,16 @@ public:
      *  Resized window callback
      */
     void resized(int w, int h);
+
+    /*
+     *  Callback for mouse movement
+     */
+    void mouseMove(double x, double y);
+
+    /*
+     *  Mouse press callback
+     */
+    void mouseButton(int button, int action, int mods);
 
 protected:
     explicit Window(GLFWwindow* window);
@@ -37,6 +51,12 @@ protected:
      */
     glm::mat4 proj() const;
 
+    /*
+     *  View matrix
+     *  Applies scale, rotation, offset
+     */
+    glm::mat4 view() const;
+
     /*  Pointer to raw window  */
     GLFWwindow* const window;
 
@@ -45,5 +65,20 @@ protected:
     int width;
     int height;
 
+    /*  Store the scene's center, scale, and rotation                    *
+     *  (Euler angles aren't the best representation, but they're easy)  */
+    glm::vec3 center;
+    float scale=1;
+    float pitch=0;
+    float  roll=0;
+
+    enum { WINDOW_DRAG_NONE,
+           WINDOW_DRAG_PAN,
+           WINDOW_DRAG_ROTATE} drag_mode=WINDOW_DRAG_NONE;
+
+    /*  This is the current mouse position (in -1, 1 window coordinates)  */
+    glm::vec2 mouse_pos;
+
+    /*  Objects to draw in 3D viewport  */
     Axes axes;
 };
--- lib/src/ui/window.cpp
+++ lib/src/ui/window.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include "ao/ui/window.hpp"
 #include "ao/core/tree.hpp"
@@ -33,6 +34,16 @@ static void _resized(GLFWwindow* window, int w, int h)
     static_cast<Window*>(glfwGetWindowUserPointer(window))->resized(w, h);
 }
 
+static void _mouseMove(GLFWwindow* window, double x, double y)
+{
+    static_cast<Window*>(glfwGetWindowUserPointer(window))->mouseMove(x, y);
+}
+
+static void _mouseButton(GLFWwindow* window, int b, int a, int m)
+{
+    static_cast<Window*>(glfwGetWindowUserPointer(window))->mouseButton(b, a, m);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 Window::Window(GLFWwindow* window)
@@ -40,7 +51,10 @@ Window::Window(GLFWwindow* window)
 {
     glfwGetFramebufferSize(window, &width, &height);
     glfwSetWindowUserPointer(window, this);
+
     glfwSetFramebufferSizeCallback(window, _resized);
+    glfwSetCursorPosCallback(window, _mouseMove);
+    glfwSetMouseButtonCallback(window, _mouseButton);
 }
 
 void Window::resized(int w, int h)
@@ -50,25 +64,87 @@ void Window::resized(int w, int h)
     draw();
 }
 
-glm::mat4 Window::proj() const
+void Window::mouseMove(double x, double y)
 {
-    glm::mat4 m;
-    if (width > height)
+    int w;
+    int h;
+    glfwGetWindowSize(window, &w, &h);
+
+    // Scale coordinates to the range -1, 1
+    auto new_pos = glm::vec2((2*x)/w - 1, 1 - (2*y)/h);
+
+    // If we're panning, adjust the center position
+    if (drag_mode == WINDOW_DRAG_PAN)
     {
-        m = glm::scale(m, {height/float(width), 1.0, 1.0});
+        // Find the starting position in world coordinates
+        auto inv = glm::inverse(proj() * view());
+        auto diff = inv * glm::vec4(new_pos, 0.0, 1.0) -
+                    inv * glm::vec4(mouse_pos, 0.0, 1.0);
+
+        center += glm::vec3(diff.x, diff.y, diff.z);
+    }
+
+    mouse_pos = new_pos;
+
+    if (drag_mode != WINDOW_DRAG_NONE)
+    {
+        draw();
+    }
+}
+
+void Window::mouseButton(int button, int action, int mods)
+{
+    (void)mods;
+
+    if (action == GLFW_PRESS)
+    {
+        if (button == GLFW_MOUSE_BUTTON_1)
+        {
+            drag_mode = WINDOW_DRAG_PAN;
+        }
+        else if (button == GLFW_MOUSE_BUTTON_2)
+        {
+            drag_mode = WINDOW_DRAG_ROTATE;
+        }
     }
     else
     {
-        m = glm::scale(m, {1.0, width/float(height), 1.0});
+        drag_mode = WINDOW_DRAG_NONE;
     }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+glm::mat4 Window::proj() const
+{
+    if (width > height)
+    {
+        return glm::scale(glm::vec3(height/float(width), 1.0, 1.0));
+    }
+    else
+    {
+        return glm::scale(glm::vec3(1.0, width/float(height), 1.0));
+    }
+}
+
+glm::mat4 Window::view() const
+{
+    glm::mat4 m;
+    m = glm::scale(m, {scale, scale, scale});
+    m = glm::translate(m, center);
+
     return m;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 void Window::draw() const
 {
+    auto m = proj() * view();
+
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    axes.draw(proj());
+    axes.draw(m);
 
     glfwSwapBuffers(window);
 }
