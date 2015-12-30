9b47bf3ffff80a6d0f0eff3d24a1d5ac82764e5c
Matt Keeter <matt.j.keeter@gmail.com>
2015-12-29 20:31:53 -0500

    Use a nicer fragment shader

--- lib/src/gl/frame.cpp
+++ lib/src/gl/frame.cpp
@@ -38,10 +38,24 @@ uniform sampler2D norm;
 
 out vec4 fragColor;
 
+vec4 shade(vec3 norm)
+{
+    vec3 light = vec3(0.99f, 0.96f, 0.89f);
+    vec3 dark  = vec3(0.20f, 0.25f, 0.30f);
+
+    // a is a light pointed directly down
+    float a = dot(norm, vec3(0.0f,   0.0f, 1.0f));
+
+    // b is a light coming in from an angle
+    float b = dot(norm, vec3(0.57f, 0.57f, 0.57f));
+
+    return vec4((a*light + (1.0f - a)*dark)*0.35f +
+                (b*light + (1.0f - b)*dark)*0.65f, 1.0f);
+}
+
 void main()
 {
     float d = texture(depth, tex_coord).r;
-    vec3 n = texture(norm, tex_coord).rgb - vec3(0.5f);
     if (isinf(d))
     {
         discard;
@@ -50,7 +64,7 @@ void main()
     {
         float z = (1.0f - d) / 2.0f;
         gl_FragDepth = z;
-        fragColor = m * vec4(2.0f * abs(n), 1.0f);
+        fragColor = shade(2.0f * (texture(norm, tex_coord).rgb - 0.5f));
     }
 }
 )";
