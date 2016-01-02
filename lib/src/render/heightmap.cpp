60ca1f6228cd87d0ca9bd6996ca07f2d45a565f6
Matt Keeter <matt.j.keeter@gmail.com>
2016-01-01 19:40:49 -0500

    Factor common functionality into a helper struct

--- lib/src/render/heightmap.cpp
+++ lib/src/render/heightmap.cpp
@@ -9,29 +9,80 @@
 namespace Heightmap
 {
 
+////////////////////////////////////////////////////////////////////////////////
+
 /*
-*  Helper functions that calculates normals for a region of the image
-*/
-static void normals(Tree* t, NormalImage& norm,
-                    size_t* xs, size_t* ys, size_t num)
+ *  Helper class that stores a queue of points to get normals for
+ */
+struct NormalRenderer
 {
-    const Gradient* gs = t->evalCore<Gradient>(num);
-    for (size_t i=0; i < num; ++i)
+    NormalRenderer(Tree* t, const Region& r, NormalImage& norm)
+        : t(t), r(r), norm(norm) {}
+
+    /*
+     *  Assert on destruction that the normals were flushed
+     */
+    ~NormalRenderer()
     {
-        // Find the normal's length (to normalize it)
-        double len = sqrt(pow(gs[i].dx, 2) +
-                          pow(gs[i].dy, 2) +
-                          pow(gs[i].dz, 2));
-
-        // Pack each normal into the 0-255 range
-        uint32_t dx = 255 * (gs[i].dx / (2 * len) + 0.5);
-        uint32_t dy = 255 * (gs[i].dy / (2 * len) + 0.5);
-        uint32_t dz = 255 * (gs[i].dz / (2 * len) + 0.5);
-
-        // Pack the normals and a dummy alpha byte into the image
-        norm(ys[i], xs[i]) = (0xff << 24) | (dz << 16) | (dy << 8) | dx;
+        assert(count == 0);
     }
-}
+
+    void run()
+    {
+        const Gradient* gs = t->evalCore<Gradient>(count);
+        for (size_t i=0; i < count; ++i)
+        {
+            // Find the normal's length (to normalize it)
+            double len = sqrt(pow(gs[i].dx, 2) +
+                              pow(gs[i].dy, 2) +
+                              pow(gs[i].dz, 2));
+
+            // Pack each normal into the 0-255 range
+            uint32_t dx = 255 * (gs[i].dx / (2 * len) + 0.5);
+            uint32_t dy = 255 * (gs[i].dy / (2 * len) + 0.5);
+            uint32_t dz = 255 * (gs[i].dz / (2 * len) + 0.5);
+
+            // Pack the normals and a dummy alpha byte into the image
+            norm(ys[i], xs[i]) = (0xff << 24) | (dz << 16) | (dy << 8) | dx;
+        }
+        count = 0;
+    }
+
+    void flush()
+    {
+        if (count > 0)
+        {
+            run();
+        }
+    }
+
+    void push(size_t i, size_t j, double z)
+    {
+        xs[count] = r.X.min + i;
+        ys[count] = r.Y.min + j;
+        t->setPoint<Gradient>(Gradient(r.X.pos(i), 1, 0, 0),
+                              Gradient(r.Y.pos(j), 0, 1, 0),
+                              Gradient(z, 0, 0, 1),
+                              count++);
+
+        // If the gradient array is completely full, execute a
+        // calculation that finds normals and blits them to the image
+        if (count == NUM_POINTS)
+        {
+            run();
+        }
+    }
+
+    Tree* t;
+    const Region& r;
+    NormalImage& norm;
+
+    // Store the x, y coordinates of rendered points for normal calculations
+    static constexpr size_t NUM_POINTS = Result::count<Gradient>();
+    size_t xs[NUM_POINTS];
+    size_t ys[NUM_POINTS];
+    size_t count = 0;
+};
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -44,11 +95,8 @@ static void pixels(Tree* t, const Region& r, DepthImage& depth, NormalImage& nor
 
     int index = 0;
 
-    // Store the x, y coordinates of rendered points for normal calculations
-    constexpr size_t NUM_POINTS = Result::count<Gradient>();
-    size_t xs[NUM_POINTS];
-    size_t ys[NUM_POINTS];
-    size_t norm_count = 0;
+    // Helper struct to render normals
+    NormalRenderer nr(t, r, norm);
 
     // Unflatten results into the image, breaking out of loops early when a pixel
     // is written (because all subsequent pixels will be below it).  This
@@ -68,31 +116,15 @@ static void pixels(Tree* t, const Region& r, DepthImage& depth, NormalImage& nor
                 // this z-column (since future voxels are behind this one)
                 index += r.Z.size - k - 1;
 
-                // Store information for rendering gradients in bulk
-                xs[norm_count] = r.X.min + i;
-                ys[norm_count] = r.Y.min + j;
-                t->setPoint<Gradient>(Gradient(r.X.pos(i), 1, 0, 0),
-                                      Gradient(r.Y.pos(j), 0, 1, 0),
-                                      Gradient(z, 0, 0, 1),
-                                      norm_count++);
-                // If the gradient array is completely full, execute a
-                // calculation that finds normals and blits them to the image
-                if (norm_count == NUM_POINTS)
-                {
-                    normals(t, norm, xs, ys, norm_count);
-                    norm_count = 0;
-                }
-
+                // Store normals to render in a bulk pass
+                nr.push(i, j, z);
                 break;
             }
         }
     }
 
     // Render the last of the normal calculations
-    if (norm_count > 0)
-    {
-        normals(t, norm, xs, ys, norm_count);
-    }
+    nr.flush();
 }
 
 /*
@@ -104,15 +136,12 @@ static void pixels(Tree* t, const Region& r, DepthImage& depth, NormalImage& nor
 static void fill(Tree* t, const Region& r, DepthImage& depth,
                  NormalImage& norm)
 {
-    // Store the x, y coordinates of rendered points for normal calculations
-    constexpr size_t NUM_POINTS = Result::count<Gradient>();
-    size_t xs[NUM_POINTS];
-    size_t ys[NUM_POINTS];
-    size_t norm_count = 0;
-
     // Store the maximum z position (which is what we're flooding into
     // the depth image)
     const double z = r.Z.pos(r.Z.size - 1);
+
+    // Helper struct to handle normal rendering
+    NormalRenderer nr(t, r, norm);
 
     // Iterate over every pixel in the region
     for (unsigned i=0; i < r.X.size; ++i)
@@ -123,31 +152,13 @@ static void fill(Tree* t, const Region& r, DepthImage& depth,
             if (depth(r.Y.min + j, r.X.min + i) < z)
             {
                 depth(r.Y.min + j, r.X.min + i) = z;
-
-                // Store information for rendering gradients in bulk
-                xs[norm_count] = r.X.min + i;
-                ys[norm_count] = r.Y.min + j;
-                t->setPoint<Gradient>(Gradient(r.X.pos(i), 1, 0, 0),
-                                      Gradient(r.Y.pos(j), 0, 1, 0),
-                                      Gradient(z, 0, 0, 1),
-                                      norm_count++);
-
-                // If the gradient array is completely full, execute a
-                // calculation that finds normals and blits them to the image
-                if (norm_count == NUM_POINTS)
-                {
-                    normals(t, norm, xs, ys, norm_count);
-                    norm_count = 0;
-                }
+                nr.push(i, j, z);
             }
         }
     }
 
     // Render the last of the normal calculations
-    if (norm_count > 0)
-    {
-        normals(t, norm, xs, ys, norm_count);
-    }
+    nr.flush();
 }
 
 /*
