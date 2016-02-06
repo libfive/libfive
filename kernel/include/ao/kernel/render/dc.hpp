35c359185400aea1cd736599eb3f032c724c505b
Matt Keeter <matt.j.keeter@gmail.com>
2016-02-06 16:47:49 -0500

    Move QEF into separate file (not yet checked in)

--- kernel/include/ao/kernel/render/dc.hpp
+++ kernel/include/ao/kernel/render/dc.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <Eigen/Dense>
 #include <glm/vec3.hpp>
 
 #include "ao/kernel/format/mesh.hpp"
@@ -20,46 +19,6 @@ namespace DC
      *  a triangle mesh.
      */
     Mesh Render(Tree* t, const Region& r);
-
-    /*
-     *  A QEF structure represents an quadratic error function
-     */
-    struct QEF
-    {
-        /*  The A matrix is of the form
-         *  [n1x, n1y, n1z]
-         *  [n2x, n2y, n2z]
-         *  [n3x, n3y, n3z]
-         *  ...
-         *  (with one row for each Hermite intersection)
-         */
-        Eigen::Matrix<float, Eigen::Dynamic, 3> A;
-
-        /*  The B matrix is of the form
-         *  [p1x . n1x]
-         *  [p2x . n2x]
-         *  [p3x . n3x]
-         *  ...
-         *  (with one row for each Hermite intersection)
-         */
-        Eigen::Matrix<float, Eigen::Dynamic, 1> b;
-
-        /*
-         *  operator() evaluates the QEF
-         */
-        Eigen::Vector3f operator()(const Eigen::Vector3f& pt) const;
-
-        /*
-         *  minimize solves the QEF for a value near the given center
-         */
-        Eigen::Vector3f minimize(const Eigen::Vector3f& center) const;
-    };
-
-    /*
-     *  Returns a quadratic error function based on surface intersections
-     *  and normals along the octree's edges
-     */
-    QEF getQEF(const Octree* o, Evaluator* e);
 
     /*
      *  Checks to see whether a cell should be collapsed (using its QEF)
