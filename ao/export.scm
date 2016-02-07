853b4a41f4b76be2b7a08a536ca9028c2fb90e20
Matt Keeter <matt.j.keeter@gmail.com>
2016-02-07 15:26:47 -0500

    ao-export-mesh binding

--- ao/export.scm
+++ ao/export.scm
@@ -7,3 +7,7 @@
 X, Y, Z are '(min max) intervals; res is a resolution in voxels per unit."
     (tree_export_heightmap (jit shape) file X Y Z res))
 
+(define-public (ao-export-mesh shape file X Y Z res)
+    " Renders a shape and saves it to an mesh
+X, Y, Z are '(min max) intervals; res is a resolution in voxels per unit."
+    (tree_export_mesh (jit shape) file X Y Z res))
