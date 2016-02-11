879a3da21dfaa9a422f0b0735d374b3b3c0b1ac5
Matt Keeter <matt.j.keeter@gmail.com>
2016-02-10 20:43:54 -0500

    Tweak Interval typedef and remove rounding shenanigans

--- kernel/include/ao/kernel/eval/interval.hpp
+++ kernel/include/ao/kernel/eval/interval.hpp
@@ -2,4 +2,8 @@
 
 #include <boost/numeric/interval.hpp>
 
-typedef boost::numeric::interval<float> Interval;
+typedef boost::numeric::interval<float,
+        boost::numeric::interval_lib::policies<
+            boost::numeric::interval_lib::rounded_transc_opp<float>,
+            boost::numeric::interval_lib::checking_catch_nan<float>>>
+        Interval;
