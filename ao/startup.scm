e2e1bd857d1460029b7021a6c442f4c2e3d78baf
Matt Keeter <matt.j.keeter@gmail.com>
2016-02-15 19:34:46 -0500

    Helper function to list shapes

--- ao/startup.scm
+++ ao/startup.scm
@@ -1,3 +1,13 @@
 (add-to-load-path (string-append (dirname (current-filename)) "/.."))
 (use-modules (ao overload) (ao shapes) (ao core) (ao export))
 (system "clear")
+
+(define (ao-shapes)
+    (let ((docs
+        (module-map (lambda (sym var)
+            (let ((v (variable-ref var)))
+            (if (procedure? v) (procedure-documentation v))))
+            (resolve-interface '(ao shapes)))))
+    (map (lambda (d) (display d) (newline))
+         (sort (filter string? docs) string<?)))
+    #t)
