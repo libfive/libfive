4e95477b471adcbb6d79c00caba3169158362824
Matt Keeter <matt.j.keeter@gmail.com>
2016-03-19 15:06:13 -0400

    Create affine X, Y, Z tokens in UI

--- bind/api.cpp
+++ bind/api.cpp
@@ -81,9 +81,9 @@ int opcode_enum(char* op)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-Token* token_x(Store* s) { return s->X(); }
-Token* token_y(Store* s) { return s->Y(); }
-Token* token_z(Store* s) { return s->Z(); }
+Token* token_x(Store* s) { return s->affine(1, 0, 0, 0); }
+Token* token_y(Store* s) { return s->affine(0, 1, 0, 0); }
+Token* token_z(Store* s) { return s->affine(0, 0, 1, 0); }
 
 Token* token_const(Store* s, float f)   { return s->constant(f); }
 
