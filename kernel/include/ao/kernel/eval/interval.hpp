1aa5fc547740ea5b253d48edfe4b3c8fb832245a
Matt Keeter <matt.j.keeter@gmail.com>
2016-09-26 21:35:57 -0400

    Tweak rounding modes to fix rendering bugs

--- kernel/include/ao/kernel/eval/interval.hpp
+++ kernel/include/ao/kernel/eval/interval.hpp
@@ -24,8 +24,8 @@
 
 typedef boost::numeric::interval<float,
     boost::numeric::interval_lib::policies<
-        boost::numeric::interval_lib::save_state_nothing<
-            boost::numeric::interval_lib::rounded_transc_exact<float>>,
+        boost::numeric::interval_lib::save_state<
+            boost::numeric::interval_lib::rounded_transc_std<float>>,
         boost::numeric::interval_lib::checking_base<float>>> Interval;
 
 inline Interval atan2(const Interval& y, const Interval& x)
