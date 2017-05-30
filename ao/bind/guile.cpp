75929e29d5d044fd725b61c919372901dd2dc995
Matt Keeter <matt.j.keeter@gmail.com>
2017-05-30 08:43:00 -0400

    Minimal extension that can be loaded into Guile

--- ao/bind/guile.cpp
+++ ao/bind/guile.cpp
@@ -2,12 +2,27 @@
 #include <cstdlib>
 #include "lib.h"
 
+void del_tree(void* t)      { ao_tree_delete((ao_tree)t); }
+SCM make_tree(ao_tree t)    { return scm_from_pointer(t, del_tree); }
+
 void init_ao(void*)
 {
+    scm_c_eval_string(R"(
+(use-modules (system foreign))
 
+(define-wrapped-pointer-type
+    tree tree? wrap-tree unwrap-tree
+    (lambda (o p)
+        (format p "#<tree 0x~x>"
+        (pointer-address (unwrap-tree o))))) )");
 }
+
+extern "C"
+{
 
 void scm_init_ao()
 {
     scm_c_define_module("ao", init_ao, NULL);
 }
+
+}   // extern C
