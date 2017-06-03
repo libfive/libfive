5580b050eddc7b24d6488ee7635e35661f95e860
Matt Keeter <matt.j.keeter@gmail.com>
2017-06-03 12:14:16 -0400

    Add error handling to evaluator

--- gui/src/interpreter.cpp
+++ gui/src/interpreter.cpp
@@ -1,3 +1,4 @@
+#include <QDebug>
 #include <libguile.h>
 #include "gui/interpreter.hpp"
 
@@ -26,8 +27,28 @@ void Interpreter::onScriptChanged(QString s)
     timer.start(200);
 }
 
+static SCM eval(void* body) {
+    QString& input = *(QString*)body;
+    auto out = scm_c_eval_string(input.toLocal8Bit().data());
+    return scm_simple_format(SCM_BOOL_F, scm_from_locale_string("~A"),
+            scm_list_1(out));
+}
+
+static SCM handler(void* data, SCM key, SCM args)
+{
+    *(bool*)data = false;
+    return scm_simple_format(SCM_BOOL_F, scm_from_locale_string("~A: ~A"),
+           scm_list_2(key, scm_simple_format(
+                SCM_BOOL_F, scm_cadr(args), scm_caddr(args))));
+}
+
 void Interpreter::evalScript()
 {
-    auto out = scm_c_eval_string(script.toLocal8Bit().data());
-    scm_simple_format(SCM_BOOL_T, scm_from_locale_string("~A"), scm_list_1(out));
+    bool success = true;
+    auto str = scm_to_locale_string(
+            scm_internal_catch(SCM_BOOL_T, eval, &script, handler, &success));
+    QString out(str);
+    free(str);
+
+    qDebug() << success << out;
 }
