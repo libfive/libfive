db7de0c8a4cf012cd57d19a60df3cbe4272cea72
Matt Keeter <matt.j.keeter@gmail.com>
2017-06-27 18:14:01 -0400

    More octree tests

--- ao/test/octree.cpp
+++ ao/test/octree.cpp
@@ -135,3 +135,77 @@ TEST_CASE("Vertex positioning on sliced sphere")
 
     REQUIRE(get_error(out, t) < 0.001);
 }
+
+TEST_CASE("Feature detection with coincident faces")
+{
+    auto t = max(max(circle(0.5), -Tree::Z() - 0.5),
+            -0.5 - Tree::Z());
+    Region r({-1, 1}, {-1, 1}, {-1, 1}, 4);
+
+    WARN("REQUIRE(false) to prevent crash");
+    REQUIRE(false);
+    std::unique_ptr<Octree> out(Octree::render(t, r));
+    REQUIRE(true);
+}
+
+TEST_CASE("Vertex positioning on extruded circle")
+{
+    auto t = max(max(circle(0.5), -Tree::Z() - 0.5),
+            -0.5 + Tree::Z());
+    Region r({0, 1}, {0, 1}, {0, 1}, 4);
+
+    std::unique_ptr<Octree> out(Octree::render(t, r));
+
+    // Walk every leaf node in the octree, keeping track of the
+    // minimum and maximum vertex radius
+    float rmax = -std::numeric_limits<float>::infinity();
+    float rmin =  std::numeric_limits<float>::infinity();
+
+    // Queue of octrees to process
+    std::list<const Octree*> targets = {out.get()};
+    while (targets.size())
+    {
+        const Octree* o = targets.front();
+        targets.pop_front();
+
+        if (o->getType() == Octree::BRANCH)
+        {
+            for (unsigned i=0; i < 8; ++i)
+            {
+                targets.push_back(o->child(i));
+            }
+        }
+        else if (o->getType() == Octree::LEAF)
+        {
+            auto vert = o->getVertex();
+            float r = sqrt(pow(vert.x(), 2) + pow(vert.y(), 2));
+            rmax = std::max(r, rmax);
+            rmin = std::max(r, rmin);
+        }
+    }
+
+    REQUIRE(rmin > 0.4);
+    REQUIRE(rmax < 0.6);
+}
+
+TEST_CASE("Octree collapsing on extruded circle")
+{
+    auto t = max(circle(1), Tree::Z());
+    Region r({0, 1}, {0, 1}, {0, 1}, 2);
+
+    std::unique_ptr<Octree> out(Octree::render(t, r));
+    REQUIRE(out->getType() == out->BRANCH);
+}
+
+TEST_CASE("Octree vertex location on circle edge")
+{
+    auto t = max(circle(1), Tree::Z());
+    Region r({0.5, 1}, {0.5, 1}, {0, 0.5}, 2);
+
+    std::unique_ptr<Octree> out(Octree::render(t, r));
+    REQUIRE(out->getType() == out->LEAF);
+    auto v = out->getVertex();
+    CAPTURE(v);
+    REQUIRE(v.norm() > 0.9);
+    REQUIRE(v.norm() < 1.1);
+}
