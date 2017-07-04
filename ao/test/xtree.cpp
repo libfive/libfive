0be9daeb67f5d19aceccca5d1277ccae3c97f05a
Matt Keeter <matt.j.keeter@gmail.com>
2017-07-04 10:54:23 -0400

    Write unit tests for XTree vertex positioning

--- ao/test/xtree.cpp
+++ ao/test/xtree.cpp
@@ -7,8 +7,24 @@ using namespace Kernel;
 
 TEST_CASE("XTree<2>()")
 {
-    Evaluator e(min(Tree::X(), -Tree::X() + 0.2));
+    SECTION("Vertex positioning (with two planes)")
+    {
+        Evaluator a(min(Tree::X(), -Tree::X() + 0.2));
+        auto ta = XTree<2>(&a, Region<2>({-1, -1}, {1, 1}));
+        REQUIRE(ta.vert.x() == Approx(0.1));
+        REQUIRE(ta.vert.y() == Approx(0.0));
 
-    auto t = XTree<2>(&e, Region<2>({-1, -1}, {1, 1}));
-    REQUIRE(true);
+        Evaluator b(max(Tree::X(), -Tree::X() + 0.2));
+        auto tb = XTree<2>(&a, Region<2>({-1, -1}, {1, 1}));
+        REQUIRE(tb.vert.x() == Approx(0.1));
+        REQUIRE(tb.vert.y() == Approx(0.0));
+    }
+
+    SECTION("Snapping to cell center")
+    {
+        Evaluator a(Tree::X());
+        auto ta = XTree<2>(&a, Region<2>({10, 13}, {12, 15}));
+        REQUIRE(ta.vert.x() == Approx(11));
+        REQUIRE(ta.vert.y() == Approx(14));
+    }
 }
