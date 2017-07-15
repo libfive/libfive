29723412439e1fd607b2918ad76b2c4c87e9483c
Matt Keeter <matt.j.keeter@gmail.com>
2017-07-15 18:11:03 -0400

    Begin writing 3D dual grid walker

--- ao/include/ao/render/axes.hpp
+++ ao/include/ao/render/axes.hpp
@@ -7,8 +7,18 @@ namespace Axis {
     enum Axis { X = 1, Y = 2, Z = 4 };
 
     /*  a, Q[a], R[a] is right-handed */
-    extern Axis Q[3];
-    extern Axis R[3];
+    inline constexpr Axis Q(Axis a)
+    {
+        return (a == Axis::X) ? Axis::Y :
+               (a == Axis::Y) ? Axis::Z
+                              : Axis::X;
+    }
+    inline constexpr Axis R(Axis a)
+    {
+        return (a == Axis::X) ? Axis::Z :
+               (a == Axis::Y) ? Axis::X
+                              : Axis::Y;
+    }
 
 }   // namespace Axis
 }   // namespace Kernel
--- ao/include/ao/render/brep/dual.hpp
+++ ao/include/ao/render/brep/dual.hpp
@@ -64,10 +64,7 @@ void Dual<2>::walk(XTree<2>& t, V& v)
         // Recurse down every subface in the quadtree
         for (auto& c : t.children)
         {
-            if (c.get())
-            {
-                walk(*c, v);
-            }
+            walk(*c, v);
         }
 
         //  Then, call edge on every pair of cells
@@ -83,13 +80,96 @@ void Dual<2>::walk(XTree<2>& t, V& v)
 }
 ////////////////////////////////////////////////////////////////////////////////
 
+template <typename V, Axis::Axis A>
+void edge3(XTree<3>& a, XTree<3>& b, XTree<3>& c, XTree<3>& d, V& v)
+{
+    if (a.isBranch() || b.isBranch() || c.isBranch() || d.isBranch())
+    {
+        constexpr auto Q = Axis::Q(A);
+        constexpr auto R = Axis::R(A);
+
+        edge3<V, A>(a.child(Q|R), b.child(R), c.child(Q), d.child(0), v);
+        edge3<V, A>(a.child(Q|R|A), b.child(R|A), c.child(Q|A), d.child(A), v);
+    }
+}
+
+template <typename V, Axis::Axis A>
+void face3(XTree<3>& a, XTree<3>& b, V& v)
+{
+    if (a.isBranch() || b.isBranch())
+    {
+        constexpr auto Q = Axis::Q(A);
+        constexpr auto R = Axis::R(A);
+
+        face3<V, A>(a.child(A), b.child(0), v);
+        face3<V, A>(a.child(Q|A), b.child(Q), v);
+        face3<V, A>(a.child(R|A), b.child(R), v);
+        face3<V, A>(a.child(Q|R|A), b.child(Q|R), v);
+
+        edge3<V, Q>(a.child(A), a.child(R|A), b.child(0), b.child(R), v);
+        edge3<V, Q>(a.child(Q|A), a.child(Q|R|A), b.child(Q), b.child(Q|R), v);
+
+        edge3<V, R>(a.child(A), b.child(0), a.child(A|Q), b.child(Q), v);
+        edge3<V, R>(a.child(R|A), b.child(R), a.child(R|A|Q), b.child(R|Q), v);
+    }
+}
+
 template <>
 template <typename V>
 void Dual<3>::walk(XTree<3>& t, V& v)
 {
-    (void)t;
-    (void)v;
-    // TODO
+    if (t.isBranch())
+    {
+        // Recurse, calling the cell procedure for every child
+        for (auto& c : t.children)
+        {
+            walk(*c, v);
+        }
+
+        // Then call the face procedure on every pair of cells
+        face3<V, Axis::X>(t.child(0), t.child(Axis::X), v);
+        face3<V, Axis::X>(t.child(Axis::Y), t.child(Axis::Y | Axis::X), v);
+        face3<V, Axis::X>(t.child(Axis::Z), t.child(Axis::Z | Axis::X), v);
+        face3<V, Axis::X>(t.child(Axis::Y | Axis::Z), t.child(Axis::Y | Axis::Z | Axis::X), v);
+
+        face3<V, Axis::Y>(t.child(0), t.child(Axis::Y), v);
+        face3<V, Axis::Y>(t.child(Axis::X), t.child(Axis::X | Axis::Y), v);
+        face3<V, Axis::Y>(t.child(Axis::Z), t.child(Axis::Z | Axis::Y), v);
+        face3<V, Axis::Y>(t.child(Axis::X | Axis::Z), t.child(Axis::X | Axis::Z | Axis::Y), v);
+
+        face3<V, Axis::Z>(t.child(0), t.child(Axis::Z), v);
+        face3<V, Axis::Z>(t.child(Axis::X), t.child(Axis::X | Axis::Z), v);
+        face3<V, Axis::Z>(t.child(Axis::Y), t.child(Axis::Y | Axis::Z), v);
+        face3<V, Axis::Z>(t.child(Axis::X | Axis::Y), t.child(Axis::X | Axis::Y | Axis::Z), v);
+
+        // Finally, call the edge function 6 times
+        edge3<V, Axis::Z>(t.child(0),
+             t.child(Axis::X),
+             t.child(Axis::Y),
+             t.child(Axis::X | Axis::Y), v);
+        edge3<V, Axis::Z>(t.child(Axis::Z),
+             t.child(Axis::X | Axis::Z),
+             t.child(Axis::Y | Axis::Z),
+             t.child(Axis::X | Axis::Y | Axis::Z), v);
+
+        edge3<V, Axis::X>(t.child(0),
+             t.child(Axis::Y),
+             t.child(Axis::Z),
+             t.child(Axis::Y | Axis::Z), v);
+        edge3<V, Axis::X>(t.child(Axis::X),
+             t.child(Axis::Y | Axis::X),
+             t.child(Axis::Z | Axis::X),
+             t.child(Axis::Y | Axis::Z | Axis::X), v);
+
+        edge3<V, Axis::Y>(t.child(0),
+             t.child(Axis::Z),
+             t.child(Axis::X),
+             t.child(Axis::Z | Axis::X), v);
+        edge3<V, Axis::Y>(t.child(Axis::Y),
+             t.child(Axis::Z | Axis::Y),
+             t.child(Axis::X | Axis::Y),
+             t.child(Axis::Z | Axis::X | Axis::Y), v);
+    }
 }
 
 }   // namespace Kernel
