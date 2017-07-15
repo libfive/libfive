b8dbc737ca8b07363634063b1810366613eca8d1
Matt Keeter <matt.j.keeter@gmail.com>
2017-07-15 18:48:21 -0400

    Add 3D dual grid vertex handling
    Hard-coded per-axis cases after drawing them out on paper.

--- ao/include/ao/render/brep/dual.hpp
+++ ao/include/ao/render/brep/dual.hpp
@@ -80,6 +80,28 @@ void Dual<2>::walk(XTree<2>& t, V& v)
 }
 ////////////////////////////////////////////////////////////////////////////////
 
+template <typename V>
+void vert3(XTree<3>& a, XTree<3>& b, XTree<3>& c, XTree<3>& d,
+           XTree<3>& e, XTree<3>& f, XTree<3>& g, XTree<3>& h, V& v)
+{
+    if (a.isBranch() || b.isBranch() || c.isBranch() || d.isBranch() ||
+        e.isBranch() || f.isBranch() || g.isBranch() || h.isBranch())
+    {
+        vert3(a.child(Axis::X | Axis::Y | Axis::Z),
+              b.child(Axis::Y | Axis::Z),
+              c.child(Axis::X | Axis::Z),
+              d.child(Axis::Z),
+              e.child(Axis::X | Axis::Y),
+              f.child(Axis::Y),
+              g.child(Axis::X),
+              h.child(0), v);
+    }
+    else
+    {
+        v({{&a, &b, &c, &d, &e, &f, &g, &h}});
+    }
+}
+
 template <typename V, Axis::Axis A>
 void edge3(XTree<3>& a, XTree<3>& b, XTree<3>& c, XTree<3>& d, V& v)
 {
@@ -90,6 +112,28 @@ void edge3(XTree<3>& a, XTree<3>& b, XTree<3>& c, XTree<3>& d, V& v)
 
         edge3<V, A>(a.child(Q|R), b.child(R), c.child(Q), d.child(0), v);
         edge3<V, A>(a.child(Q|R|A), b.child(R|A), c.child(Q|A), d.child(A), v);
+
+        if (A == Axis::X)
+        {
+            vert3<V>(a.child(Axis::Y|Axis::Z), a.child(Axis::Y|Axis::Z|Axis::X),
+                     b.child(Axis::Z), b.child(Axis::Z|Axis::X),
+                     c.child(Axis::Y), c.child(Axis::Y|Axis::X),
+                     d.child(0), c.child(Axis::X), v);
+        }
+        else if (A == Axis::Y)
+        {
+            vert3<V>(a.child(Axis::X|Axis::Z), c.child(Axis::Z),
+                     a.child(Axis::X|Axis::Y|Axis::Z), c.child(Axis::Y|Axis::Z),
+                     b.child(Axis::X), d.child(0),
+                     b.child(Axis::X|Axis::Y), d.child(Axis::Y), v);
+        }
+        else if (A == Axis::Z)
+        {
+            vert3<V>(a.child(Axis::X|Axis::Y), b.child(Axis::Y),
+                     c.child(Axis::X), d.child(0),
+                     a.child(Axis::X|Axis::Y|Axis::Z), b.child(Axis::Y|Axis::Z),
+                     c.child(Axis::X|Axis::Z), d.child(Axis::Z), v);
+        }
     }
 }
 
@@ -111,6 +155,28 @@ void face3(XTree<3>& a, XTree<3>& b, V& v)
 
         edge3<V, R>(a.child(A), b.child(0), a.child(A|Q), b.child(Q), v);
         edge3<V, R>(a.child(R|A), b.child(R), a.child(R|A|Q), b.child(R|Q), v);
+
+        if (A == Axis::X)
+        {
+            vert3<V>(a.child(Axis::X), b.child(0),
+                     a.child(Axis::X|Axis::Y), b.child(Axis::Y),
+                     a.child(Axis::X|Axis::Z), b.child(Axis::Z),
+                     a.child(Axis::X|Axis::Y|Axis::Z), b.child(Axis::Y|Axis::Z), v);
+        }
+        else if (A == Axis::Y)
+        {
+            vert3<V>(a.child(Axis::Y), a.child(Axis::X|Axis::Y),
+                     b.child(0), b.child(Axis::X),
+                     a.child(Axis::Y|Axis::Z), a.child(Axis::X|Axis::Y|Axis::Z),
+                     b.child(Axis::Z), b.child(Axis::X|Axis::Z), v);
+        }
+        else if (A == Axis::Z)
+        {
+            vert3<V>(a.child(Axis::Z), a.child(Axis::X|Axis::Z),
+                     a.child(Axis::Y|Axis::Z), a.child(Axis::X|Axis::Y|Axis::Z),
+                     b.child(0), b.child(Axis::X),
+                     b.child(Axis::Y), b.child(Axis::X|Axis::Y), v);
+        }
     }
 }
 
@@ -142,7 +208,7 @@ void Dual<3>::walk(XTree<3>& t, V& v)
         face3<V, Axis::Z>(t.child(Axis::Y), t.child(Axis::Y | Axis::Z), v);
         face3<V, Axis::Z>(t.child(Axis::X | Axis::Y), t.child(Axis::X | Axis::Y | Axis::Z), v);
 
-        // Finally, call the edge function 6 times
+        // Call the edge function 6 times
         edge3<V, Axis::Z>(t.child(0),
              t.child(Axis::X),
              t.child(Axis::Y),
@@ -169,6 +235,13 @@ void Dual<3>::walk(XTree<3>& t, V& v)
              t.child(Axis::Z | Axis::Y),
              t.child(Axis::X | Axis::Y),
              t.child(Axis::Z | Axis::X | Axis::Y), v);
+
+        // Finally, recurse down towards the center of the cell
+        vert3(*t.children[0],                *t.children[Axis::X],
+              *t.children[Axis::Y],          *t.children[Axis::X|Axis::Y],
+              *t.children[Axis::Z],          *t.children[Axis::X|Axis::Z],
+              *t.children[Axis::Y|Axis::Z],  *t.children[Axis::X|Axis::Y|Axis::Z], v);
+
     }
 }
 
