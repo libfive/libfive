1672e8d65348f86d84433a23afc516cee3536cf7
Matt Keeter <matt.j.keeter@gmail.com>
2017-07-18 21:20:42 -0400

    More test cases for dual walking

--- ao/test/dual.cpp
+++ ao/test/dual.cpp
@@ -43,14 +43,19 @@ struct Walker3
 
         // Confirm that we're unpacking vertices in the right order
         REQUIRE(a[0]->vert.x() <= a[1]->vert.x());
-        REQUIRE(a[0]->vert.y() <= a[2]->vert.y());
         REQUIRE(a[2]->vert.x() <= a[3]->vert.x());
-        REQUIRE(a[1]->vert.y() <= a[3]->vert.y());
-
         REQUIRE(a[4]->vert.x() <= a[5]->vert.x());
-        REQUIRE(a[4]->vert.y() <= a[6]->vert.y());
         REQUIRE(a[6]->vert.x() <= a[7]->vert.x());
+
+        REQUIRE(a[0]->vert.y() <= a[2]->vert.y());
+        REQUIRE(a[1]->vert.y() <= a[3]->vert.y());
+        REQUIRE(a[4]->vert.y() <= a[6]->vert.y());
         REQUIRE(a[5]->vert.y() <= a[7]->vert.y());
+
+        REQUIRE(a[0]->vert.z() <= a[4]->vert.z());
+        REQUIRE(a[1]->vert.z() <= a[5]->vert.z());
+        REQUIRE(a[2]->vert.z() <= a[6]->vert.z());
+        REQUIRE(a[3]->vert.z() <= a[7]->vert.z());
     }
 };
 
