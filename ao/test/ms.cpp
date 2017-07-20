cc24ab32eb53de2b319c5b4aa9f5e34e1b55451f
Matt Keeter <matt.j.keeter@gmail.com>
2017-07-19 21:06:10 -0400

    Recursive trees aren't being contoured correctly

--- ao/test/ms.cpp
+++ ao/test/ms.cpp
@@ -7,7 +7,7 @@
 
 using namespace Kernel;
 
-TEST_CASE("SquareMarcher: operator() on circle")
+TEST_CASE("SquareMarcher: operator() on circle (fixed depth)")
 {
     Evaluator e(circle(1));
 
@@ -28,6 +28,35 @@ TEST_CASE("SquareMarcher: operator() on circle")
         rmin = fmin(rmin, pt.norm());
         rmax = fmax(rmax, pt.norm());
     }
+    REQUIRE(rmin != 2);
+    REQUIRE(rmax != 0);
+    REQUIRE(rmin > 0.99);
+    REQUIRE(rmax < 1.01);
+}
+
+TEST_CASE("SquareMarcher: operator() on circle (recursive)")
+{
+    Evaluator e(circle(1));
+
+    // Create a padded scaffolding for the tree
+    Region<2> r({-1, -1}, {1, 1});
+    const auto scaffold = Scaffold<2>(&e, r, 2, true);
+
+    // Create the quadtree on the scaffold (without subdividing farther)
+    auto xtree = XTree<2>(&e, scaffold);
+
+    SquareMarcher ms(&e);
+    Dual<2>::walk(xtree, ms);
+
+    float rmin = 2;
+    float rmax = 0;
+    for (auto pt : ms.pts)
+    {
+        rmin = fmin(rmin, pt.norm());
+        rmax = fmax(rmax, pt.norm());
+    }
+    REQUIRE(rmin != 2);
+    REQUIRE(rmax != 0);
     REQUIRE(rmin > 0.99);
     REQUIRE(rmax < 1.01);
 }
