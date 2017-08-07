9a04abf19eebc0cc84da288d2810db4075ec068b
Matt Keeter <matt.j.keeter@gmail.com>
2017-08-06 20:56:04 -0400

    Add unit test for 3D MC table

--- ao/test/marching.cpp
+++ ao/test/marching.cpp
@@ -7,6 +7,21 @@ using namespace Kernel;
 TEST_CASE("Marching::buildTable<2>")
 {
     auto t = Marching::buildTable<2>();
+    REQUIRE(t->size() == 16);
+
+    // Make sure that we have built every item in the table
+    for (unsigned i=1; i < t->size() - 1; ++i)
+    {
+        CAPTURE(i);
+        auto& _t = (*t)[i];
+        REQUIRE((*t)[i][0][0].first != -1);
+    }
+}
+
+TEST_CASE("Marching::buildTable<3>")
+{
+    auto t = Marching::buildTable<3>();
+    REQUIRE(t->size() == 256);
 
     // Make sure that we have built every item in the table
     for (unsigned i=1; i < t->size() - 1; ++i)
