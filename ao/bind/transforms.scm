869f86da43fa615de10822384d9d2ed92c79d005
Matt Keeter <matt.j.keeter@gmail.com>
2017-09-07 20:05:09 -0400

    Add a few more transforms

--- ao/bind/transforms.scm
+++ ao/bind/transforms.scm
@@ -17,3 +17,19 @@
     "reflect-xy shape
     Moves the given shape across the plane Y=Z"
     (remap-shape (shape x y z) x z y))
+
+(define* (rotate-x shape angle #:optional center)
+    "rotate-x shape angle [#(x0 y0 z0)]
+    Rotate the given shape by an angle in radians
+    The center of rotation is 0,0,0 or specified by the optional argument"
+    (when (not center) (set! center #(0 0 0)))
+    (let ((centered (move shape (list (- (.x center))
+                                      (- (.y center))
+                                      (- (.z center)))))
+          (ca (cos angle))
+          (sa (sin angle)))
+      (move (remap-shape (centered x y z)
+            x
+            (+ (* ca y) (* sa z))
+            (+ (* (- sa) y) (* ca z))) center)))
+(export rotate-x)
