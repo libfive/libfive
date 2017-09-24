4d9c4d0105839366d3fcd478ad0c904b938a2cbd
Matt Keeter <matt.j.keeter@gmail.com>
2017-09-24 14:35:06 -0400

    Remove unnecessary semicolons

--- ao/src/tree/tree.cpp
+++ ao/src/tree/tree.cpp
@@ -208,34 +208,34 @@ void Tree::Tree_::print(std::ostream& stream, Opcode::Opcode prev_op)
 // Mass-produce definitions for overloaded operations
 #define OP_UNARY(name, opcode) \
 Kernel::Tree name(const Kernel::Tree& a) { return Kernel::Tree(opcode, a); }
-OP_UNARY(square,    Kernel::Opcode::SQUARE);
-OP_UNARY(sqrt,      Kernel::Opcode::SQRT);
+OP_UNARY(square,    Kernel::Opcode::SQUARE)
+OP_UNARY(sqrt,      Kernel::Opcode::SQRT)
 Kernel::Tree Kernel::Tree::operator-() const
     { return Kernel::Tree(Kernel::Opcode::NEG, *this); }
 Kernel::Tree abs(const Kernel::Tree& a) { return max(a, -a); }
-OP_UNARY(sin,       Kernel::Opcode::SIN);
-OP_UNARY(cos,       Kernel::Opcode::COS);
-OP_UNARY(tan,       Kernel::Opcode::TAN);
-OP_UNARY(asin,      Kernel::Opcode::ASIN);
-OP_UNARY(acos,      Kernel::Opcode::ACOS);
-OP_UNARY(atan,      Kernel::Opcode::ATAN);
-OP_UNARY(exp,       Kernel::Opcode::EXP);
+OP_UNARY(sin,       Kernel::Opcode::SIN)
+OP_UNARY(cos,       Kernel::Opcode::COS)
+OP_UNARY(tan,       Kernel::Opcode::TAN)
+OP_UNARY(asin,      Kernel::Opcode::ASIN)
+OP_UNARY(acos,      Kernel::Opcode::ACOS)
+OP_UNARY(atan,      Kernel::Opcode::ATAN)
+OP_UNARY(exp,       Kernel::Opcode::EXP)
 #undef OP_UNARY
 
 #define OP_BINARY(name, opcode) \
 Kernel::Tree name(const Kernel::Tree& a, const Kernel::Tree& b) \
     { return Kernel::Tree(opcode, a, b); }
-OP_BINARY(operator+,    Kernel::Opcode::ADD);
-OP_BINARY(operator*,    Kernel::Opcode::MUL);
-OP_BINARY(min,          Kernel::Opcode::MIN);
-OP_BINARY(max,          Kernel::Opcode::MAX);
-OP_BINARY(operator-,    Kernel::Opcode::SUB);
-OP_BINARY(operator/,    Kernel::Opcode::DIV);
-OP_BINARY(atan2,        Kernel::Opcode::ATAN2);
-OP_BINARY(pow,          Kernel::Opcode::POW);
-OP_BINARY(nth_root,     Kernel::Opcode::NTH_ROOT);
-OP_BINARY(mod,          Kernel::Opcode::MOD);
-OP_BINARY(nanfill,      Kernel::Opcode::NANFILL);
+OP_BINARY(operator+,    Kernel::Opcode::ADD)
+OP_BINARY(operator*,    Kernel::Opcode::MUL)
+OP_BINARY(min,          Kernel::Opcode::MIN)
+OP_BINARY(max,          Kernel::Opcode::MAX)
+OP_BINARY(operator-,    Kernel::Opcode::SUB)
+OP_BINARY(operator/,    Kernel::Opcode::DIV)
+OP_BINARY(atan2,        Kernel::Opcode::ATAN2)
+OP_BINARY(pow,          Kernel::Opcode::POW)
+OP_BINARY(nth_root,     Kernel::Opcode::NTH_ROOT)
+OP_BINARY(mod,          Kernel::Opcode::MOD)
+OP_BINARY(nanfill,      Kernel::Opcode::NANFILL)
 #undef OP_BINARY
 
 
