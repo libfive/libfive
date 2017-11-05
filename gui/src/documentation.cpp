655b26144f80d732409d391a225ff04d041e8cca
Matt Keeter <matt.j.keeter@gmail.com>
2017-11-05 09:35:34 -0500

    Use QTextBrowser for documentation with internal links

--- gui/src/documentation.cpp
+++ gui/src/documentation.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include <QTextEdit>
+#include <QTextBrowser>
 #include <QVBoxLayout>
 
 #include "gui/documentation.hpp"
@@ -20,41 +20,51 @@ DocumentationPane::DocumentationPane()
 
     auto layout = new QVBoxLayout();
 
-    auto txt = new QTextEdit();
-
-    txt->setFontFamily("Courier");
-
     // Flatten documentation into a single-level map
     QMap<QString, QString> fs;
+    QMap<QString, QString> tags;
     for (auto mod : docs->docs.keys())
     {
         for (auto f : docs->docs[mod].keys())
         {
             fs[f] = docs->docs[mod][f].doc;
+            tags.insert(f, "i" + QString::fromStdString(
+                        std::to_string(tags.size())));
         }
     }
 
+    // Unpack documentation into a text box
+    auto txt = new QTextBrowser();
     for (auto f : fs.keys())
     {
         const auto doc = fs[f];
 
         auto f_ = doc.count(" ") ? doc.split(" ")[0] : "";
+        txt->insertHtml("<a name=\"" + tags[f] + "\" href=\"#" + tags[f] + "\">" + f + "</a><br>");
         if (f_ != f)
         {
+            txt->insertPlainText(f);
+            txt->insertPlainText(": alias for ");
             if (fs.count(f_) != 1)
             {
                 std::cerr << "DocumentationPane: missing alias "
                           << f_.toStdString() << " for " << f.toStdString()
                           << std::endl;
+                txt->insertPlainText(f_ + " (missing)\n");
             }
-            txt->insertPlainText(f);
-            txt->insertPlainText(": alias for " + f_ + "\n\n");
+            else
+            {
+                txt->insertHtml("<a href=\"#" + tags[f_] + "\">" + f_ + "</a><br>");
+            }
+            txt->insertPlainText("\n");
         }
         else
         {
             txt->insertPlainText(doc + "\n\n");
         }
     }
+    txt->setReadOnly(true);
+    txt->setTextInteractionFlags(Qt::LinksAccessibleByMouse);
 
     layout->addWidget(txt);
 
