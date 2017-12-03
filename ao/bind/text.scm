d91d1d0b6fa4efeb04ca98fc60847ebc28aa34ed
Matt Keeter <matt.j.keeter@gmail.com>
2017-12-03 16:26:06 -0500

    Add all uppercase and punctuation chars

--- ao/bind/text.scm
+++ ao/bind/text.scm
@@ -16,21 +16,23 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 |#
-(use-modules (ao shapes) (ao csg) (ao vec) (ao transforms))
+(use-modules (ao kernel) (ao shapes) (ao csg) (ao vec) (ao transforms))
 
 (define glyphs (make-hash-table))
 (define (make-glyph! char width shape)
   (hash-set! glyphs char (cons shape width)))
+(define (glyph-shape char) (car (hash-ref glyphs char)))
+(define (glyph-width char) (cdr (hash-ref glyphs char)))
 
 ;; Glyphs are stored as (shape . width) pairs
-(make-glyph! "A" 0.8
+(make-glyph! #\A 0.8
     (union (triangle #[0 0] #[0.35 1] #[0.1 0])
            (triangle #[0.1 0] #[0.35 1] #[0.45 1])
            (triangle #[0.35 1] #[0.45 1] #[0.8 0])
            (triangle #[0.7 0] #[0.35 1] #[0.8 0])
            (rectangle #[0.2 0.3] #[0.6 0.4])))
 
-(make-glyph! "B" 0.575
+(make-glyph! #\B 0.575
     (union (intersection (union (ring #[0.3 0.725] 0.225 0.1)
                                 (ring #[0.3 0.275] 0.225 0.1))
                          (rectangle #[0.3 0] #[1 1]))
@@ -42,30 +44,30 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
                  ;; Main bar of the letter
                  (rectangle #[0 0] #[0.1 1])))
 
-(make-glyph! "C" 0.57
+(make-glyph! #\C 0.57
     (union (difference (union (ring #[0.3 0.7] 0.25 0.1)
                               (ring #[0.3 0.3] 0.25 0.1))
                        (rectangle #[0 0.3] #[0.6 0.7])
                        (triangle #[0.3 0.5] #[1 1.5] #[1 -0.5]))
            (rectangle #[0 0.3] #[0.1 0.7])))
 
-(make-glyph! "D" 0.6
+(make-glyph! #\D 0.6
     (union (rectangle #[0 0] #[0.1 1])
            (intersection (rectangle #[0 0] #[1 1])
                          (ring #[0.1 0.5] 0.45 0.1))))
 
-(make-glyph! "E" 0.6
+(make-glyph! #\E 0.6
   (union (rectangle #[0 0] #[0.1 1])
          (rectangle #[0 0] #[0.6 0.1])
          (rectangle #[0 0.9] #[0.6 1])
          (rectangle #[0 0.45] #[0.6 0.55])))
 
-(make-glyph! "F" 0.6
+(make-glyph! #\F 0.6
   (union (rectangle #[0 0] #[0.1 1])
          (rectangle #[0 0.9] #[0.6 1])
          (rectangle #[0 0.45] #[0.6 0.55])))
 
-(make-glyph! "G" 0.6
+(make-glyph! #\G 0.6
   (union (rectangle #[0 0.3] #[0.1 0.7])
          (rectangle #[0.5 0.3] #[0.6 0.4])
          (rectangle #[0.3 0.4] #[0.6 0.5])
@@ -73,32 +75,32 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
                             (ring #[0.3 0.3] 0.25 0.1))
                      (rectangle #[0 0.3] #[0.6 0.7]))))
 
-(make-glyph! "H" 0.6
+(make-glyph! #\H 0.6
   (union (rectangle #[0 0] #[0.1 1])
          (rectangle #[0.5 0] #[0.6 1])
          (rectangle #[0 0.45] #[0.6 0.55])))
 
-(make-glyph! "I" 0.5
+(make-glyph! #\I 0.5
   (union (rectangle #[0.2 0] #[0.3 1])
          (rectangle #[0 0] #[0.5 0.1])
          (rectangle #[0 0.9] #[0.5 1])))
 
-(make-glyph! "J" 0.55
+(make-glyph! #\J 0.55
   (union (rectangle #[0.45 0.275] #[0.55 1])
          (intersection (ring #[0.275 0.275] 0.225 0.1)
                        (rectangle #[0 0] #[0.55 0.275]))))
 
-(make-glyph! "K" 0.6
+(make-glyph! #\K 0.6
   (difference (rectangle #[0 0] #[0.6 1])
               (triangle #[0.1 1] #[0.5 1] #[0.1 0.6])
               (triangle #[0.5 0] #[0.1 0] #[0.1 0.4])
               (triangle #[0.6 0.95] #[0.6 0.05] #[0.18 0.5])))
 
-(make-glyph! "L" 0.6
+(make-glyph! #\L 0.6
   (union (rectangle #[0 0] #[0.1 1])
          (rectangle #[0 0] #[0.6 0.1])))
 
-(make-glyph! "M" 0.8
+(make-glyph! #\M 0.8
   (union (rectangle #[0 0] #[0.1 1])
          (rectangle #[0.7 0] #[0.8 1])
          (triangle #[0 1] #[0.1 1] #[0.45 0])
@@ -106,27 +108,27 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
          (triangle #[0.7 1] #[0.8 1] #[0.35 0])
          (triangle #[0.35 0] #[0.8 1] #[0.45 0])))
 
-(make-glyph! "N" 0.6
+(make-glyph! #\N 0.6
   (union (rectangle #[0 0] #[0.1 1])
          (rectangle #[0.5 0] #[0.6 1])
          (triangle #[0 1] #[0.1 1] #[0.6 0])
          (triangle #[0.6 0] #[0.5 0] #[0 1])))
 
-(make-glyph! "O" 0.6
+(make-glyph! #\O 0.6
   (union (rectangle #[0 0.3] #[0.1 0.7])
          (rectangle #[0.5 0.3] #[0.6 0.7])
          (difference (union (ring #[0.3 0.7] 0.25 0.1)
                             (ring #[0.3 0.3] 0.25 0.1))
                      (rectangle #[0 0.3] #[0.6 0.7]))))
 
-(make-glyph! "P" 0.575
+(make-glyph! #\P 0.575
   (union (difference (ring #[0.3 0.725] 0.225 0.1)
                      (rectangle #[0 0] #[0.3 1]))
          (rectangle #[0 0] #[0.1 1])
          (rectangle #[0.1 0.45] #[0.3 0.55])
          (rectangle #[0.1 0.9] #[0.3 1])))
 
-(make-glyph! "Q" 0.6
+(make-glyph! #\Q 0.6
   (union (rectangle #[0 0.3] #[0.1 0.7])
          (rectangle #[0.5 0.3] #[0.6 0.7])
          (difference (union (ring #[0.3 0.7] 0.25 0.1)
@@ -135,7 +137,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
          (triangle #[0.5 0.1] #[0.6 0.1] #[0.6 0])
          (triangle #[0.5 0.1] #[0.5 0.3] #[0.6 0.1])))
 
-(make-glyph! "R" 0.575
+(make-glyph! #\R 0.575
   (union (difference (ring #[0.3 0.725] 0.225 0.1)
                      (rectangle #[0 0] #[0.3 1]))
          (rectangle #[0 0] #[0.1 1])
@@ -144,22 +146,163 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
          (triangle #[0.3 0.5] #[0.4 0.5] #[0.575 0])
          (triangle #[0.475 0] #[0.3 0.5] #[0.575 0])))
 
-(make-glyph! "S" 0.55
+(make-glyph! #\S 0.55
   (let ((half (difference (ring #[0.275 0.725] 0.225 0.1)
                           (rectangle #[0.275 0.45] #[0.55 0.725]))))
     (union half (reflect-x (reflect-y half 0.5) 0.275))))
 
-(make-glyph! "T" 0.6
+(make-glyph! #\T 0.6
   (union (rectangle #[0 0.9] #[0.6 1])
          (rectangle #[0.25 0] #[0.35 1])))
 
-(make-glyph! "U" 0.6
+(make-glyph! #\U 0.6
   (union (rectangle #[0 0.3] #[0.1 1])
          (rectangle #[0.5 0.3] #[0.6 1])
          (difference (ring #[0.3 0.3] 0.25 0.1)
                      (rectangle #[0 0.3] #[0.6 0.7]))))
 
-(make-glyph! "V" 0.6
+(make-glyph! #\V 0.6
   (let ((half (union (triangle #[0 1] #[0.1 1] #[0.35 0])
                      (triangle #[0.35 0] #[0.25 0] #[0 1]))))
     (union half (reflect-x half 0.3))))
+
+(make-glyph! #\W 0.8
+  (union (triangle #[0 1] #[0.1 1] #[0.25 0])
+         (triangle #[0.25 0] #[0.15 0] #[0 1])
+         (triangle #[0.15 0] #[0.35 1] #[0.45 1])
+         (triangle #[0.45 1] #[0.25 0] #[0.15 0])))
+
+(make-glyph! #\X 0.8
+  (let ((half (union (triangle #[0 1] #[0.125 1] #[0.8 0])
+                     (triangle #[0.8 0] #[0 0.675] #[0 1]))))
+    (union half (reflect-x half))))
+
+(make-glyph! #\Y 0.8
+  (let ((half (union (triangle #[0 1] #[0.1 1] #[0.45 0.5])
+                     (triangle #[0.45 5] #[0.35 0.5] #[0 1]))))
+    (union half (reflect-x half 0.4) (rectangle #[0.35 0.45] #[0 0.5]))))
+
+(make-glyph! #\Z 0.6
+  (difference (rectangle #[0 0.6] #[0 1])
+              (triangle #[0 0.1] #[0 0.9] #[0.45 0.9])
+              (triangle #[0.6 0.1] #[0.15 0.1] #[0.6 0.9])))
+
+(make-glyph! #\space 0.55 (lambda-shape (x y z) 1))
+
+(make-glyph! #\, 0.175
+  (union (circle #[0.1 0.075] 0.075)
+    (intersection (circle #[0.075 0.075] 0.075)
+                  (rectangle #[0 0.15] #[-0.15 0.075])
+                  (inverse (triangle #[0.075 0.075] #[0 -0.15] #[-0.5 0.075])))
+))
+
+(make-glyph! #\. 0.175 (circle #[0.075 0.075] 0.075))
+
+(make-glyph! #\' 0.1 (rectangle #[0 0.1] #[0.55 0.8]))
+
+(make-glyph! #\" 0.3
+  (union (rectangle #[0 0.1] #[0.55 0.8])
+         (rectangle #[0.2 0.3] #[0.55 0.8])))
+
+(make-glyph! #\: 0.15
+  (union (circle #[0.075 0.15] 0.075)
+         (circle #[0.075 0.45] 0.075)))
+
+(make-glyph! #\; 0.15
+  (union
+    (intersection (scale-y (circle #[0.075 0.15] 0.074) 3 0.015)
+                  (rectangle #[0 0.15] #[-0.075 0.15])
+                  (inverse (triangle #[0.075 0.15] #[0 -0.075] #[-0.5 0.15])))
+    (circle #[0.075 0.45] 0.075)
+    (circle #[0.1 0.15] 0.075)))
+
+(make-glyph! #\! 0.1
+  (union (rectangle #[0.025 0.125] #[0.3 1])
+         (circle #[0.075 0.075] 0.075)))
+
+(make-glyph! #\- 0.45 (rectangle #[0.05 0.4] #[0.35 0.45]))
+
+(make-glyph! #\) 0.3
+  (scale-x (intersection (circle #[0 0.4] 0.6)
+                         (inverse (scale-x (circle #[0 0.4] 0.5) 0.7))
+                         (rectangle #[0 0.6] #[-0.2 1])) 0.5))
+
+(make-glyph! #\( 0.3
+  (scale-x (intersection (circle #[0.6 0.4] 0.6)
+                         (inverse (scale-x (circle #[0.6 0.4] 0.5) 0.7 0.6))
+                         (rectangle #[0 0.6] #[-0.2 1])) 0.5))
+
+(make-glyph! #\1 0.3
+  (difference (rectangle #[0 0] #[0.3 1])
+              (rectangle #[0 0] #[0.2 1])
+              (circle #[0 1] 0.2)))
+
+(make-glyph! #\2 0.55
+  (union (difference (ring #[0.275 0.725] 0.275 0.175)
+                     (rectangle #[0 0] #[0.55 0.725]))
+         (rectangle #[0 0] #[0.55 0.1])
+         (triangle #[0 0.1] #[0.45 0.775] #[0.55 0.725])
+         (triangle #[0 0.1] #[0.55 0.725] #[0.125 0.1])))
+
+(make-glyph! #\3 0.55
+  (difference
+    (union (ring #[0.3 0.725] 0.275 0.175)
+           (ring #[0.3 0.225] 0.275 0.175))
+    (rectangle #[0 0.275] #[0.275 0.725])))
+
+(make-glyph! #\4 0.5
+  (intersection (union (triangle #[-0.10 0.45] #[0.4 1] #[0.4 0.45])
+                       (rectangle #[0.4 0] #[0.5 1]))
+              (inverse (triangle #[0.4 0.85] #[0.4 0.55] #[0.1 0.55]))
+              (rectangle #[0 0] #[0.5 1])))
+
+(make-glyph! #\5 0.65
+  (union (difference (ring #[0.325 0.325] 0.325 0.225)
+                     (rectangle #[0 0.325] #[0.325 0.65]))
+         (rectangle #[0 0.55] #[0.325 0.65])
+         (rectangle #[0 0.55] #[0.1 1])
+         (rectangle #[0.1 0.65] #[0.9 1])))
+
+(make-glyph! #\6 0.55
+  (let ((hook
+    (intersection
+      (circle #[0.275 0.725] 0.275)
+      (inverse (scale-y (circle #[0.275 0.725] 0.175) 1.2 0.725))
+      (rectangle #[0 0.725] #[0.55 1])
+      (inverse (triangle #[0.275 0.925] #[0.55 0.9] #[0.55 0.725])))))
+  (union (ring #[0.275 0.275] 0.275 0.175)
+         (rectangle #[0 0.275] #[0.1 0.45])
+         (difference (scale-x (scale-y hook 2 1) 1.1)
+                     (rectangle #[0.275 0.65] #[0 0.7])))))
+
+(make-glyph! #\7 0.6
+  (union (rectangle #[0 0.9] #[0.6 1])
+         (triangle #[0 0] #[0.475 0.9] #[0.6 0.9])
+         (triangle #[0 0] #[0.6 0.9] #[0.125 0])))
+
+(make-glyph! #\8 0.55
+  (union (ring #[0.3 0.725] 0.275 0.175)
+         (ring #[0.3 0.225] 0.275 0.175)))
+
+(make-glyph! #\9 (glyph-width #\6)
+  (reflect-x (reflect-y (glyph-shape #\6) 0.5)
+             (/ (glyph-width #\6) 2)))
+
+(make-glyph! #\0 0.7
+  (scale-x (difference (circle #[0.5 0.5] 0.5)
+                       (scale-x (circle #[0.5 0.5] 0.4) (sqrt 0.7) 0.5))
+           0.7))
+
+(make-glyph! #\+ 0.55
+  (union (rectangle #[0 0.45] #[0.5 0.55])
+         (rectangle #[0.2 0.25] #[0.3 0.75])))
+
+(make-glyph! #\/ 0.55
+  (union (triangle #[0 0] #[0.425 1] #[0.55 1])
+         (triangle #[0 0] #[0.55 1] #[0.125 0])))
+
+(make-glyph! #\? 0.55
+  (intersection (difference (ring #[0.275 0.725] 0.275 0.175)
+                            (rectangle #[0 0.45] #[0.275 0.725]))
+                (rectangle #[0.225 0.3] #[0.325 0.55])
+                (circle #[0.275 0.075] 0.075)))
