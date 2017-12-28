e8cca265a9d3b9a4cb64d3aceccd8a51b1957e7c
Matt Keeter <matt.j.keeter@gmail.com>
2017-12-28 15:02:00 -0500

    Behave correctly if not in a Git checkout
    Closes issue #71

--- ao/src/version.cmake
+++ ao/src/version.cmake
@@ -1,18 +1,29 @@
 execute_process(COMMAND git log --pretty=format:'%h' -n 1
-                OUTPUT_VARIABLE GIT_REV)
-execute_process(COMMAND bash -c "git diff --quiet --exit-code || echo +"
-                OUTPUT_VARIABLE GIT_DIFF)
-execute_process(COMMAND git describe --exact-match --tags
-                OUTPUT_VARIABLE GIT_TAG
+                OUTPUT_VARIABLE GIT_REV
                 ERROR_QUIET)
-execute_process(COMMAND git rev-parse --abbrev-ref HEAD
-                OUTPUT_VARIABLE GIT_BRANCH)
 
-string(STRIP "${GIT_REV}" GIT_REV)
-string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
-string(STRIP "${GIT_DIFF}" GIT_DIFF)
-string(STRIP "${GIT_TAG}" GIT_TAG)
-string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+# Check whether we got any revision (which isn't always the case, e.g.
+# when someone downloaded a zip file from Github instead of a checkout)
+if ("${GIT_REV}" STREQUAL "")
+    set(GIT_REV "N/A")
+    set(GIT_DIFF "")
+    set(GIT_TAG "N/A")
+    set(GIT_BRANCH "N/A")
+else()
+    execute_process(COMMAND bash -c "git diff --quiet --exit-code || echo +"
+                    OUTPUT_VARIABLE GIT_DIFF)
+    execute_process(COMMAND git describe --exact-match --tags
+                    OUTPUT_VARIABLE GIT_TAG
+                    ERROR_QUIET)
+    execute_process(COMMAND git rev-parse --abbrev-ref HEAD
+                    OUTPUT_VARIABLE GIT_BRANCH)
+
+    string(STRIP "${GIT_REV}" GIT_REV)
+    string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
+    string(STRIP "${GIT_DIFF}" GIT_DIFF)
+    string(STRIP "${GIT_TAG}" GIT_TAG)
+    string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+endif()
 
 set(VERSION "const char* GIT_REV=\"${GIT_REV}${GIT_DIFF}\";
 const char* GIT_TAG=\"${GIT_TAG}\";
