87cb8dd87fa63eacacb253cd65cf3eff5d429f23
Rémi Gillig <remigillig@gmail.com>
2018-02-05 20:33:22 +0000

    Add support for MSVC+vcpkg in CMake.

--- libfive/src/render/brep/xtree.cpp
+++ libfive/src/render/brep/xtree.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <Eigen/StdVector>
 
+#include "libfive/export.hpp"
 #include "libfive/render/brep/xtree.hpp"
 #include "libfive/render/axes.hpp"
 
