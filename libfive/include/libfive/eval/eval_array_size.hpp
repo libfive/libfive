a50fbcbae229716cf1a3fd73f5b78c0e39ffa674
Matt Keeter <matt.j.keeter@gmail.com>
2018-02-19 18:29:18 -0500

    Add missing headers

---
+++ libfive/include/libfive/eval/eval_array_size.hpp
@@ -0,0 +1,4 @@
+// This definition lives in its own header so we can include it
+// in both eval_array.hpp and oracle.hpp, since having either
+// include the other causes horrible include loops.
+#define LIBFIVE_EVAL_ARRAY_SIZE 256
