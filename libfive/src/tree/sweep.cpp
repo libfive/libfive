1dba76ee52e058e90a70192f458a75bf3badbfcd
Matt Keeter <matt.j.keeter@gmail.com>
2018-03-11 19:30:33 -0400

    Add untested functions for sweeps

---
+++ libfive/src/tree/sweep.cpp
@@ -0,0 +1,42 @@
+/*
+libfive: a CAD kernel for modeling with implicit functions
+Copyright (C) 2017  Matt Keeter
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <array>
+
+#include "libfive/tree/sweep.hpp"
+#include "libfive/tree/oracle_clause_bezier.hpp"
+
+using namespace Kernel;
+
+Tree sweep(Tree input, const Eigen::Vector3f& a,
+                       const Eigen::Vector3f& b,
+                       const Eigen::Vector3f& c)
+{
+    Tree t(std::unique_ptr<BezierClosestPointOracleClause>(
+            new BezierClosestPointOracleClause(a, b, c)));
+
+    std::vector<Tree> remapped;
+    for (unsigned i=0; i < 3; ++i)
+    {
+        remapped.push_back(pow(1 - t, 2) * a(i) +
+                           2 * (1 - t) * t * b(i) +
+                           pow(t, 2) * c(i));
+    }
+
+    return input.remap(remapped[0], remapped[1], remapped[2]);
+}
