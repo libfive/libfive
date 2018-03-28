4c86910a4af4f5d7e20b08564746142efa14b373
Matt Keeter <matt.j.keeter@gmail.com>
2018-03-27 21:00:53 -0400

    Begin working on N-dimensional neighbor tracking

---
+++ libfive/include/libfive/render/brep/neighbors.hpp
@@ -0,0 +1,153 @@
+/*
+libfive: a CAD kernel for modeling with implicit functions
+Copyright (C) 2018  Matt Keeter
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#pragma once
+
+#include <array>
+
+#include "libfive/render/brep/marching.hpp"
+#include "libfive/render/brep/xtree.hpp"
+
+namespace Kernel {
+
+template <unsigned N>
+class Neighbors
+{
+public:
+    /*
+     *  Constructor, returning an empty neighbor array
+     */
+    Neighbors() {
+        populatePositions();
+        std::fill(neighbors.begin(), neighbors.end(), nullptr);
+    }
+
+    /*
+     *  Looks up the given corner to see if it has already been calculated
+     *  by any of the neighbors, returning FILLED / EMPTY if that is the case
+     *  and AMBIGUOUS otherwise.
+     */
+    Interval::State check(uint8_t corner)
+    {
+        for (unsigned i=0; i < _pow(i, 3) - 1; ++i)
+        {
+            if (neighbors[i] != nullptr &&
+                invert(floating[i]) ==
+                (fixed[i]^ invert(corner)))
+            {
+                return neighbors[i]->corners[
+                    (invert(corner) & (~floating[i])) |
+                    (corner         &   floating[i])];
+            }
+        }
+        return Interval::AMBIGUOUS;
+    }
+
+    Neighbors<N> push(uint8_t child,
+            const std::array<std::unique_ptr<const XTree<N>>, 1 << N>&
+                children)
+    {
+        Neighbors out;
+        for (unsigned i=0; i < _pow(3, N) - 1; ++i)
+        {
+            // If the neighbor is destined to come from within the array
+            // of children, then pick it out in this conditional.
+            if ((fixed[i]^ child) == invert(floating[i]))
+            {
+                out.neighbors[i] = children[fixed[i]|
+                                (floating[i] & child)];
+            }
+            // Otherwise, it is destined to come from one of the higher-level
+            // neighbors, calculated in this branch.
+            else
+            {
+                // Figure out which higher-level neighbor we should index into
+                auto target_floating = floating[i] | (child ^ fixed[i]);
+                auto target_fixed = child & target_floating;
+
+                // Look up the index of this higher-level neighbor
+                auto index = (target_fixed << N) | target_floating;
+                unsigned j = remap[index];
+                assert(j != 0xFF);
+
+                // Check to see if the neighbor has children
+                if (neighbors[j])
+                {
+                    uint8_t c = 0; // TODO
+                    out.neighbors[i] = neighbors[j]->children[c];
+                }
+            }
+
+        }
+        return out;
+    }
+
+protected:
+    static void populatePositions() {
+        // Early exit if already populated
+        if (loaded)
+        {
+            return;
+        }
+        std::fill(remap.begin(), remap.end(), 0xFF);
+
+        for (unsigned i=0; i < _pow(3, N) - 1; ++i)
+        {
+            // Ternary decoding loop
+            unsigned j=i;
+            for (unsigned axis=0; axis < N; ++axis)
+            {
+                switch (j % 3) {
+                    case 1: fixed[i]    |= (1 << axis); break;
+                    case 2: floating[i] |= (1 << axis); break;
+                    default: continue;
+                }
+                j /= 3;
+            }
+
+            // Store the remapping lookup here as well
+            remap[(fixed[i] << N) | floating[i]] = i;
+        }
+        loaded = true;
+    }
+
+    static constexpr uint8_t invert(uint8_t in) {
+        return (~in) & mask();
+    }
+    static constexpr uint8_t mask() {
+        return (1 << N) - 1;
+    }
+
+    /*  bitfield representing direction for non-floating axes
+     *  floating axes have their relevant bit set to 0, but you
+     *  need to use the floating field to decode */
+    static std::array<bool, _pow(3, N) - 1> fixed;
+
+    /*  bitfield representing which axes are floating  */
+    static std::array<bool, _pow(3, N) - 1> floating;
+
+    static bool loaded;
+
+    /*  remap[(fixed << N) | floating] returns the index into
+     *  the fixed/floating arrays with the given bitfields.  */
+    static std::array<uint8_t, 1 << (2 * N)> remap;
+
+    std::array<XTree<N>*, _pow(3, N) - 1> neighbors;
+};
+
+}   // namespace Kernel
