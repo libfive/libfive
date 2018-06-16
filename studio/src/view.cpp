9217aa1c5a0ab4aa8801e26e741e994f865bb971
Matt Keeter <matt.j.keeter@gmail.com>
2018-06-16 12:10:38 -0400

    Fix issue when shapes are swapped mid-drag

--- studio/src/view.cpp
+++ studio/src/view.cpp
@@ -72,6 +72,13 @@ void View::setShapes(QList<Shape*> new_shapes)
         auto n = new_shapes_map.find((*itr)->id());
         if (n == new_shapes_map.end())
         {
+            if (*itr == drag_target)
+            {
+                drag_target->setGrabbed(false);
+                drag_target = nullptr;
+                emit(dragEnd());
+                mouse.state = mouse.RELEASED;
+            }
             disconnect(*itr, &Shape::redraw, this, &View::update);
             (*itr)->deleteLater();
             itr = shapes.erase(itr);
