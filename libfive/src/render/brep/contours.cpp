217b99dfe2acf6c9829b4d31611071ea10d8efce
Matt Keeter <matt.j.keeter@gmail.com>
2018-11-22 12:26:01 -0500

    Move collection to Contours class

--- libfive/src/render/brep/contours.cpp
+++ libfive/src/render/brep/contours.cpp
@@ -20,7 +20,7 @@ namespace Kernel {
 class DCSegments
 {
 public:
-    using Output = BRep<2>;
+    using Output = Contours;
     using Input = DCTree<2>;
 
     DCSegments(PerThreadBRep<2>& m) : m(m) {}
@@ -141,104 +141,11 @@ std::unique_ptr<Contours> Contours::render(
         return nullptr;
     }
 
-    // Perform marching squares
-    auto segs = Dual<2>::walk<DCSegments>(xtree, workers, cancel,
-                                          EMPTY_PROGRESS_CALLBACK);
-
-    auto c = std::unique_ptr<Contours>(new Contours(r));
-
-    // Maps from index (in ms) to item in segments vector
-    std::map<uint32_t, uint32_t> heads;
-    std::map<uint32_t, uint32_t> tails;
-    std::vector<std::list<uint32_t>> contours;
-
-    for (auto& s : segs->branes)
-    {
-        if (cancel == true) {
-            return nullptr;
-        }
-
-        {   // Check to see whether we can attach to the back of a tail
-            auto t = tails.find(s[0]);
-            if (t != tails.end())
-            {
-                contours[t->second].push_back(s[1]);
-                tails.insert({s[1], t->second});
-                tails.erase(t);
-                continue;
-            }
-        }
-
-        {   // Otherwise, see if we should prepend ourselves to a head
-            auto h = heads.find(s[1]);
-            if (h != heads.end())
-            {
-                contours[h->second].push_front(s[0]);
-                heads.insert({s[0], h->second});
-                heads.erase(h);
-                continue;
-            }
-        }
-
-        // Otherwise, start a new multi-segment line
-        heads[s[0]] = contours.size();
-        tails[s[1]] = contours.size();
-        contours.push_back({s[0], s[1]});
-    }
-
-    std::vector<bool> processed(contours.size(), false);
-    for (unsigned i=0; i < contours.size(); ++i)
-    {
-        if (cancel == true) {
-            return nullptr;
-        }
-
-        if (processed[i])
-        {
-            continue;
-        }
-        c->contours.push_back(std::vector<Eigen::Vector2f>());
-
-        // Weld multiple contours together here
-        unsigned target = i;
-        while (true)
-        {
-            if (cancel == true) {
-                return nullptr;
-            }
-            for (const auto& pt : contours[target])
-            {
-                c->contours.back().push_back(segs->verts[pt]);
-            }
-            processed[target] = true;
-
-            // Check for the next potentially-connected contour
-            auto h = heads.find(contours[target].back());
-            if (h != heads.end() && !processed[h->second])
-            {
-                // Remove the back point, beause it will be re-inserted
-                // as the first point in the next contour
-                c->contours.back().pop_back();
-                target = h->second;
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
-
-    return c;
-}
-
-std::unique_ptr<Contours> Contours::render(const Tree t,
-                                           const Region<2>& r,
-                                           double min_feature /*= 0.1*/,
-                                           double max_err /*= 1e-8*/,
-                                           bool multithread /*= true*/)
-{
-    std::atomic_bool cancel(false);
-    return render(t, r, min_feature, max_err, cancel, multithread ? 1 : 8);
+    // Perform marching squares, collecting into Contours
+    auto cs = Dual<2>::walk<DCSegments>(xtree, workers, cancel,
+                                        EMPTY_PROGRESS_CALLBACK);
+    cs->bbox = r;
+    return cs;
 }
 
 bool Contours::saveSVG(const std::string& filename)
@@ -299,5 +206,91 @@ bool Contours::saveSVG(const std::string& filename)
     return true;
 }
 
+void Contours::collect(const std::vector<PerThreadBRep<2>>& children)
+{
+    // First, collect into a single b-rep, with unique indices
+    BRep<2> segs;
+    segs.collect(children);
+
+    // Maps from index to item in segments vector
+    std::map<uint32_t, uint32_t> heads;
+    std::map<uint32_t, uint32_t> tails;
+    std::vector<std::list<uint32_t>> contours;
+
+    for (auto& s : segs.branes)
+    {
+        {   // Check to see whether we can attach to the back of a tail
+            auto t = tails.find(s[0]);
+            if (t != tails.end())
+            {
+                contours[t->second].push_back(s[1]);
+                tails.insert({s[1], t->second});
+                tails.erase(t);
+                continue;
+            }
+        }
+
+        {   // Otherwise, see if we should prepend ourselves to a head
+            auto h = heads.find(s[1]);
+            if (h != heads.end())
+            {
+                contours[h->second].push_front(s[0]);
+                heads.insert({s[0], h->second});
+                heads.erase(h);
+                continue;
+            }
+        }
+
+        // Otherwise, start a new multi-segment line
+        heads[s[0]] = contours.size();
+        tails[s[1]] = contours.size();
+        contours.push_back({s[0], s[1]});
+    }
+
+    std::vector<bool> processed(contours.size(), false);
+    for (unsigned i=0; i < contours.size(); ++i)
+    {
+        if (processed[i])
+        {
+            continue;
+        }
+        this->contours.push_back(std::vector<Eigen::Vector2f>());
+
+        // Weld multiple contours together here
+        unsigned target = i;
+        while (true)
+        {
+            for (const auto& pt : contours[target])
+            {
+                this->contours.back().push_back(segs.verts[pt]);
+            }
+            processed[target] = true;
+
+            // Check for the next potentially-connected contour
+            auto h = heads.find(contours[target].back());
+            if (h != heads.end() && !processed[h->second])
+            {
+                // Remove the back point, beause it will be re-inserted
+                // as the first point in the next contour
+                this->contours.back().pop_back();
+                target = h->second;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+std::unique_ptr<Contours> Contours::render(const Tree t,
+                                           const Region<2>& r,
+                                           double min_feature /*= 0.1*/,
+                                           double max_err /*= 1e-8*/,
+                                           bool multithread /*= true*/)
+{
+    std::atomic_bool cancel(false);
+    return render(t, r, min_feature, max_err, cancel, multithread ? 1 : 8);
+}
 
 }   // namespace Kernel
