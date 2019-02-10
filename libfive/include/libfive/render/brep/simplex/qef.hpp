119ea77e3f97ecc97799616c9ddaab76f42f6f2b
Matt Keeter <matt.j.keeter@gmail.com>
2019-02-10 15:30:47 -0500

    Tweak shrink value to fix unit test

--- libfive/include/libfive/render/brep/simplex/qef.hpp
+++ libfive/include/libfive/render/brep/simplex/qef.hpp
@@ -265,7 +265,7 @@ public:
      *  This is implemented by walking down in dimensionality from N to 0,
      *  picking the lowest-error solution available that is within the bounds.
      */
-    Solution solveBounded(const Region<N>& region, double shrink=(1 - 1e-6))
+    Solution solveBounded(const Region<N>& region, double shrink=(1 - 1e-9))
     {
         return solveBounded(region, shrink,
                             (region.lower + region.upper) / 2.0,
