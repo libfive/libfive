30203ffd710101c7811283e14f16a975b15c2dd7
Matt Keeter <matt.j.keeter@gmail.com>
2021-02-12 21:04:57 -0500

    Remove unmaintained Travis CI and Dockerfile

--- .travis/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -evuo pipefail
-
-cd $(dirname $0)
-docker build -t libfive:1 .
--- .travis/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -evuo pipefail
-
-export CLICOLOR_FORCE=1
-mkdir -p /build
-cd /build
-cmake /src
-make -j2
-/build/libfive/test/libfive-test --use-colour yes
