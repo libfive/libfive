4f59b953510c6961f19322478c4196ffd61bd73f
Matt Keeter <matt.j.keeter@gmail.com>
2021-02-15 19:22:44 -0500

    Get Studio compiling, although not running

--- studio/src/interpreter.cpp
+++ studio/src/interpreter.cpp
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "studio/documentation.hpp"
 #include "studio/shape.hpp"
 
-#include "libfive-guile.h"
+#include "libfive.h"
 
 _Interpreter::_Interpreter()
 {
@@ -41,12 +41,21 @@ void _Interpreter::init()
 #endif
 
     scm_init_guile();
-    scm_init_libfive_modules();
+
+    // TODO: edit path so that stuff works
 
     scm_c_use_module("libfive kernel");
     scm_eval_sandboxed = scm_c_eval_string(R"(
 (use-modules (libfive sandbox))
 eval-sandboxed
+)");
+    scm_shape_to_ptr = scm_c_eval_string(R"(
+(use-modules (libfive kernel))
+shape->ptr
+)");
+    scm_is_shape = scm_c_eval_string(R"(
+(use-modules (libfive kernel))
+shape?
 )");
 
     scm_port_eof_p = scm_c_eval_string(R"(
@@ -63,7 +72,8 @@ port-eof?
     scm_numerical_overflow_fmt = scm_from_locale_string("~A: ~A in ~A");
 
     // Protect all of our interpreter vars from garbage collection
-    for (auto s : {scm_eval_sandboxed, scm_port_eof_p, scm_valid_sym,
+    for (auto s : {scm_eval_sandboxed, scm_shape_to_ptr, scm_is_shape,
+                   scm_port_eof_p, scm_valid_sym,
                    scm_syntax_error_sym, scm_numerical_overflow_sym,
                    scm_result_fmt, scm_syntax_error_fmt,
                    scm_numerical_overflow_fmt, scm_other_error_fmt,
@@ -228,7 +238,7 @@ void _Interpreter::eval()
             {
                 auto data = scm_cdar(v);
                 auto id = static_cast<libfive::Tree::Id>(
-                        libfive_tree_id(scm_shape_to_raw_tree(scm_car(data))));
+                    scm_to_pointer(scm_call_1(scm_shape_to_ptr, scm_car(data))));
                 auto value = scm_to_double(scm_cadr(data));
                 vars[id] = value;
 
@@ -244,9 +254,10 @@ void _Interpreter::eval()
         {
             for (auto r = scm_cdar(result); !scm_is_null(r); r = scm_cdr(r))
             {
-                if (scm_is_shape(scm_car(r)))
+                if (scm_is_true(scm_call_1(scm_is_shape, scm_car(r))))
                 {
-                    auto tree = scm_shape_to_raw_tree(scm_car(r));
+                    auto tree = static_cast<libfive_tree>(
+                        scm_to_pointer(scm_call_1(scm_shape_to_ptr, scm_car(r))));
                     auto shape = new Shape(libfive::Tree(tree), vars);
                     shape->moveToThread(QApplication::instance()->thread());
                     shapes.push_back(shape);
