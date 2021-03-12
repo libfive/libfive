0b4b86012837c1d6770135e954923b9776d258cf
Matt Keeter <matt.j.keeter@gmail.com>
2021-03-12 08:34:44 -0500

    Formatting

--- studio/src/syntax.cpp
+++ studio/src/syntax.cpp
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 namespace Studio {
 
 Syntax::Syntax(QTextDocument* doc)
-        : QSyntaxHighlighter(doc)
+    : QSyntaxHighlighter(doc)
 {
     // Nothing to do here
 }
