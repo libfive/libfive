a5f2df2843387b35f40c02db2c26c9382f51b317
Matt Keeter <matt.j.keeter@gmail.com>
2021-03-23 08:48:25 -0400

    Some Python formatter implementation

--- studio/src/python/formatter.cpp
+++ studio/src/python/formatter.cpp
@@ -25,7 +25,101 @@ namespace Python {
 
 void Formatter::keyPressEvent(QPlainTextEdit* doc, QKeyEvent* e)
 {
-    e->ignore();
+    // Mostly copied from guile/formatter.cpp.  We accept the event here,
+    // then ignore it below if it doesn't match any special cases.
+    e->accept();
+
+    auto c = doc->textCursor();
+    if (e->key() == Qt::Key_Tab) {
+        // Tab does block-level indentation if we've selected text
+        if (c.hasSelection())
+        {
+            auto a = c.selectionStart();
+            auto b = c.selectionEnd();
+
+            c.setPosition(a);
+            c.beginEditBlock();
+            while (true)
+            {
+                c.movePosition(QTextCursor::StartOfLine);
+                c.insertText("    ");
+                c.movePosition(QTextCursor::Down);
+
+                if (c.position() == a || c.position() > b)
+                {
+                    break;
+                }
+                a = c.position();
+            }
+            c.endEditBlock();
+        }
+        // Otherwise, it does aligned 4-space indentation
+        else
+        {
+            doc->insertPlainText(QString(4 - (c.columnNumber() & 3), ' '));
+        }
+    }
+    // Backtab does block-level de-indentation
+    else if (e->key() == Qt::Key_Backtab && c.hasSelection()) {
+        auto a = c.selectionStart();
+        auto b = c.selectionEnd();
+
+        c.setPosition(a);
+        c.beginEditBlock();
+        while (true)
+        {
+            c.movePosition(QTextCursor::StartOfLine);
+            for (unsigned i=0; i < 4; ++i)
+            {
+                c.movePosition(QTextCursor::Right,
+                               QTextCursor::KeepAnchor);
+                if (c.selectedText() == " ")
+                {
+                    c.removeSelectedText();
+                }
+            }
+            c.movePosition(QTextCursor::Down);
+
+            if (c.position() == a || c.position() > b)
+            {
+                break;
+            }
+            a = c.position();
+        }
+        c.endEditBlock();
+    }
+    // TODO: smarter indentation on return
+    else if (e->key() == Qt::Key_Return && !c.hasSelection()) {
+        e->ignore();
+    }
+    // Backspace does 4-space aligned deletion of spaces, and deletes
+    // a single character otherwise.
+    else if (e->key() == Qt::Key_Backspace && !c.hasSelection()) {
+        // Delete one character
+        bool was_space;
+        {
+            auto c_ = doc->textCursor();
+            c_.movePosition(QTextCursor::Left, QTextCursor::KeepAnchor);
+            was_space = (c_.selectedText() == " ");
+        }
+        c.deletePreviousChar();
+
+        // Delete up to three more spaces, to column-align indentation
+        for (unsigned i=0; was_space && i < 3; ++i) {
+            if ((c.columnNumber() & 3) && c.columnNumber() != 0) {
+                c.movePosition(QTextCursor::Left, QTextCursor::KeepAnchor);
+                if (c.selectedText() == " ") {
+                    c.deletePreviousChar();
+                } else {
+                    was_space = false;
+                }
+            }
+        }
+    }
+    else
+    {
+        e->ignore();
+    }
 }
 
 }   // namespace Python
